0a3743019fe8c0dc17d779685a224ee0daf76512
Richard <richytong@gmail.com>
2020-11-05 16:01:21 -0800

    redis sorted set

--- RedisSortedSet.js
+++ RedisSortedSet.js
@@ -21,7 +21,7 @@ const {
   trace,
 } = rubicoX
 
-const isBinary = ArrayBuffer.isView
+const arrayBufferIsView = ArrayBuffer.isView
 
 const arrayFrom = Array.from
 
@@ -38,33 +38,46 @@ const slice = (from, to) => value => value.slice(from, to < 0 ? value.length + t
  * @synopsis
  * ```coffeescript [specscript]
  * RedisSortedSet(
- *   keyspace string|(any=>string),
+ *   key string|(any=>string),
  *   connection string|{ host: string, port: number, database: number },
  * ) -> redisSortedSet Object
  * ```
  *
  * @description
- * A Redis SortedSet ([redis docs](https://redis.io/topics/data-types#sorted-sets)). Supply a function as the keyspace to create a sharded collection.
+ * A Redis SortedSet ([redis docs](https://redis.io/topics/data-types#sorted-sets)). Supply a function as the key to create a sharded collection.
  *
  * ```javascript
- * RedisSortedSet('your-prefix:', 'redis://localhost:6379')
+ * RedisSortedSet('redis://localhost:6379', 'your:key')
  *
- * RedisSortedSet(item => `another-prefix:${item.id}`, {
+ * RedisSortedSet({
  *   host: 'your-domain.hash.ng.0001.use1.cache.amazonaws.com',
  *   port: 6379,
  *   database: 0,
- * })
+ * }, 'your:key')
  * ```
  */
-const RedisSortedSet = function (keyspace, connection) {
+const RedisSortedSet = function (redis, key) {
   if (this == null || this.constructor != RedisSortedSet) {
-    return new RedisSortedSet(keyspace, connection)
+    return new RedisSortedSet(redis, key)
   }
-  this.keyspace = typeof keyspace == 'function' ? keyspace : always(keyspace)
-  this.connection = typeof connection == 'string'
-    ? parseRedisConnectionString(connection)
-    : connection
-  this.redis = new Redis(this.connection)
+  if (redis == null) {
+    throw new TypeError('redis must be a Redis instance, a connection string, or a connection object')
+  }
+  if (key == null) {
+    throw new TypeError('key must be a string')
+  }
+  this.redis = redis.constructor == Redis ? redis
+    : typeof redis == 'string' ? new Redis(parseRedisConnectionString(redis))
+    : new Redis(redis)
+  this.key = key
+}
+
+RedisSortedSet.prototype.bzpopmax = function bzpopmax(...args) {
+  return this.redis.bzpopmax(this.key, ...args)
+}
+
+RedisSortedSet.prototype.bzpopmin = function bzpopmin(...args) {
+  return this.redis.bzpopmin(this.key, ...args)
 }
 
 /**
@@ -72,24 +85,118 @@ const RedisSortedSet = function (keyspace, connection) {
  *
  * @synopsis
  * ```coffeescript [specscript]
- * var option 'XX'|'NX'|'LT'|'GT'|'CH'|'WITHSCORES',
- *   options Array<option>
- *
- * RedisSortedSet(
- *   keyspace string|(any=>string),
- *   connection string|{ host: string, port: number, database: number },
- * ).zadd(
- *   score string|number,
+ * var key string,
+ *   redisConnectionString string,
+ *   redisConnection { host: string, port: number, database: number },
+ *   redis Redis,
+ *   option 'XX'|'NX'|'LT'|'GT'|'CH'|'WITHSCORES',
+ *   options Array<option>,
+ *   score number,
  *   member string,
- *   ...options
- * ) -> numElementsAddedToSortedSet Promise<number>
+ *   args Array<(score, member)|option>
+ *
+ * RedisSortedSet(redisConnectionString, key).zadd(...args) -> Promise<number>
+ *
+ * RedisSortedSet(redisConnection, key).zadd(...args) -> Promise<number>
+ *
+ * RedisSortedSet(redis, key).zadd(...args) -> Promise<number>
  * ```
  *
  * @description
- * See [redis documentation for zadd](https://redis.io/commands/zadd).
+ * See redis documentation for [zadd](https://redis.io/commands/zadd).
+ *
+ * ```javascript
+ * const mySortedSet = new RedisSortedSet('redis://localhost:6379/0', 'my:sortedSet:key')
+ *
+ * mySortedSet.zadd(1, 'one', 2, 'two', 3, 'three') // -> Promise<number>
+ * ```
  */
-RedisSortedSet.prototype.zadd = function zadd(score, member, ...options) {
-  return this.redis.zadd(this.keyspace(member), score, member, ...options)
+RedisSortedSet.prototype.zadd = function zadd(...args) {
+  return this.redis.zadd(this.key, ...args)
+}
+
+RedisSortedSet.prototype.zcard = function zcard() {
+  return this.redis.zcard(this.key)
+}
+
+RedisSortedSet.prototype.zcount = function zcount(min, max) {
+  return this.redis.zcount(this.key, min, max)
+}
+
+RedisSortedSet.prototype.zincrby = function zincrby(increment, member) {
+  return this.redis.zincrby(this.key, increment, member)
+}
+
+RedisSortedSet.prototype.zlexcount = function zlexcount(min, max) {
+  return this.redis.zlexcount(this.key, min, max)
+}
+
+RedisSortedSet.prototype.zmscore = function zmscore(member, ...members) {
+  return this.redis.zmscore(this.key, member, ...members)
+}
+
+RedisSortedSet.prototype.zpopmax = function zpopmax(count) {
+  return this.redis.zpopmax(this.key, count)
+}
+
+RedisSortedSet.prototype.zpopmin = function zpopmin(count) {
+  return this.redis.zpopmin(this.key, count)
+}
+
+RedisSortedSet.prototype.zrange = function zrange(start, stop, ...options) {
+  return this.redis.zrange(this.key, start, stop, ...options)
+}
+
+RedisSortedSet.prototype.zrangebylex = function zrangebylex(min, max, ...options) {
+  return this.redis.zrangebylex(this.key, min, max, ...options)
+}
+
+RedisSortedSet.prototype.zrangebyscore = function zrangebyscore(min, max, ...options) {
+  return this.redis.zrangebyscore(this.key, min, max, ...options)
+}
+
+RedisSortedSet.prototype.zrank = function zrank(member) {
+  return this.redis.zrank(this.key, member)
+}
+
+RedisSortedSet.prototype.zrem = function zrem(member, ...members) {
+  return this.redis.zrem(this.key, member, ...members)
+}
+
+RedisSortedSet.prototype.zremrangebylex = function zremrangebylex(min, max) {
+  return this.redis.zremrangebylex(this.key, min, max)
+}
+
+RedisSortedSet.prototype.zremrangebyrank = function zremrangebyrank(start, stop) {
+  return this.redis.zremrangebyrank(this.key, start, stop)
+}
+
+RedisSortedSet.prototype.zremrangebyscore = function zremrangebyscore(min, max) {
+  return this.redis.zremrangebyscore(this.key, min, max)
+}
+
+RedisSortedSet.prototype.zrevrange = function zrevrange(start, stop, ...options) {
+  return this.redis.zrevrange(this.key, start, stop, ...options)
+}
+
+RedisSortedSet.prototype.zrevrangebylex = function zrevrangebylex(max, min, ...options) {
+  return this.redis.zrevrangebylex(this.key, max, min, ...options)
+}
+
+RedisSortedSet.prototype.zrevrangebyscore = function zrevrangebyscore(max, min, ...options) {
+  return this.redis.zrevrangebyscore(this.key, max, min, ...options)
+}
+
+RedisSortedSet.prototype.zrevrank = function zrevrank(member) {
+  return this.redis.zrevrank(this.key, member)
+}
+
+RedisSortedSet.prototype.zscan = function zscan(cursor, ...options) {
+  return this.redis.zscan(this.key, cursor, ...options)
+}
+
+RedisSortedSet.prototype.zscore = function zscore(member) {
+  return this.redis.zscore(this.key, member)
 }
 
 module.exports = RedisSortedSet
--- RedisSortedSet.test.js
+++ RedisSortedSet.test.js
@@ -1,17 +1,128 @@
-const ThunkTest = require('thunk-test')
+const fork = require('rubico/fork')
+const Test = require('thunk-test')
 const assert = require('assert')
 const RedisSortedSet = require('./RedisSortedSet')
 
-ThunkTest('RedisSortedSet', RedisSortedSet)
-  .case('test:', 'redis://localhost:6379', async function (redisSortedSet) {
-    assert(typeof redisSortedSet == 'object')
-    assert(typeof redisSortedSet.zadd == 'function')
-    await redisSortedSet.redis.flushdb()
-    redisSortedSet.redis.disconnect()
-  })
-  .case(item => `test2:${item.id}`, 'redis://localhost:6379', async function (redisSortedSet) {
-    this.redis = redisSortedSet.redis
-    await redisSortedSet.redis.flushdb()
-    redisSortedSet.redis.disconnect()
-  })
-()
+Test('RedisSortedSet', RedisSortedSet)
+.before(function () {
+  this.redises = []
+})
+.case('redis://localhost:6379', 'test', async function (redisSortedSet) {
+  assert(typeof redisSortedSet == 'object')
+  assert(typeof redisSortedSet.zadd == 'function')
+  await redisSortedSet.redis.flushdb()
+  await Promise.all([
+    redisSortedSet.zadd(1, 'one'),
+    redisSortedSet.zadd(2, 'two'),
+    redisSortedSet.zadd(3, 'three'),
+  ])
+  assert.deepEqual(
+    await redisSortedSet.zrange(0, -1, 'WITHSCORES'),
+    ['one', '1', 'two', '2', 'three', '3'],
+  )
+  assert.deepEqual(
+    await redisSortedSet.zrangebyscore(0, 3, 'WITHSCORES'),
+    ['one', '1', 'two', '2', 'three', '3'],
+  )
+  assert.deepEqual(
+    await redisSortedSet.zrevrange(0, -1, 'WITHSCORES'),
+    ['three', '3', 'two', '2', 'one', '1'],
+  )
+  assert.deepEqual(
+    await redisSortedSet.zrevrangebyscore(3, 1, 'WITHSCORES'),
+    ['three', '3', 'two', '2', 'one', '1'],
+  )
+  assert.deepEqual(
+    await redisSortedSet.zrevrangebyscore(3, 2, 'WITHSCORES'),
+    ['three', '3', 'two', '2'],
+  )
+  await redisSortedSet.redis.flushdb()
+  await redisSortedSet.zadd(0, 'a', 1, 'b', 2, 'c')
+  assert.deepEqual(
+    await redisSortedSet.bzpopmax('does-not-exist', 0),
+    ['test', 'c', '2'],
+  )
+  assert.deepEqual(
+    await redisSortedSet.bzpopmax(0),
+    ['test', 'b', '1'],
+  )
+  assert.deepEqual(
+    await redisSortedSet.bzpopmin(20),
+    ['test', 'a', '0'],
+  )
+  assert.deepEqual(
+    await redisSortedSet.zrange(0, -1),
+    [],
+  )
+  assert.deepEqual(
+    await redisSortedSet.zrevrange(0, -1),
+    [],
+  )
+  await redisSortedSet.zadd(0, 'a', 0, 'b', 0, 'c', 0, 'd', 0, 'e', 0, 'f', 0, 'g')
+  assert.deepEqual(
+    await redisSortedSet.zrangebylex('-', '[c'),
+    ['a', 'b', 'c'],
+  )
+  assert.deepEqual(
+    await redisSortedSet.zrevrangebylex('[c', '-'),
+    ['c', 'b', 'a'],
+  )
+  assert.deepEqual(
+    await redisSortedSet.zrangebylex('-', '(c'),
+    ['a', 'b'],
+  )
+  assert.deepEqual(
+    await redisSortedSet.zrangebylex('[aaa', '(g'),
+    ['b', 'c', 'd', 'e', 'f'],
+  )
+  assert.strictEqual(
+    await redisSortedSet.zlexcount('-', '[c'),
+    3,
+  )
+  assert.strictEqual(
+    await redisSortedSet.zrank('a'),
+    0,
+  )
+  assert.strictEqual(
+    await redisSortedSet.zscore('a'),
+    '0',
+  )
+  assert.strictEqual(
+    await redisSortedSet.zrevrank('a'),
+    6,
+  )
+  await redisSortedSet.zrem('a')
+  assert.deepEqual(
+    await redisSortedSet.zrange(0, -1),
+    ['b', 'c', 'd', 'e', 'f', 'g'],
+  )
+  await redisSortedSet.zremrangebylex('(a', '(c')
+  assert.deepEqual(
+    await redisSortedSet.zrange(0, -1),
+    ['c', 'd', 'e', 'f', 'g'],
+  )
+  await redisSortedSet.zremrangebyrank(-1, -1)
+  assert.deepEqual(
+    await redisSortedSet.zrange(0, -1),
+    ['c', 'd', 'e', 'f'],
+  )
+  await redisSortedSet.zremrangebyscore(1)
+  assert.deepEqual(
+    await redisSortedSet.zrange(0, -1),
+    ['c', 'd', 'e', 'f'],
+  )
+  assert.deepEqual(
+    await redisSortedSet.zscan(0),
+    ['0', ['c', '0', 'd', '0', 'e', '0', 'f', '0']], // [cursor, [items, scores, ...]]
+  )
+  await redisSortedSet.zremrangebyscore(0)
+  assert.deepEqual(
+    await redisSortedSet.zrange(0, -1),
+    [],
+  )
+
+  this.redises.push(redisSortedSet.redis)
+})
+.after(function () {
+  this.redises.forEach(redis => redis.disconnect())
+})()
