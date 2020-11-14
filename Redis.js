1c5413df6e48a384e509c0b073c058521e3d828d
Richard <richytong@gmail.com>
2020-11-13 22:16:19 -0800

    incorporate some global redis operations

--- Redis.js
+++ Redis.js
@@ -28,4 +28,158 @@ const Redis = function (connection) {
   return this
 }
 
+/**
+ * @name Redis.prototype.expire
+ *
+ * @synopsis
+ * ```coffeescript [specscript]
+ * Redis(connection).expire(
+ *   key string,
+ *   seconds number,
+ * ) -> status Promise<'1'|'0'>
+ * ```
+ *
+ * @description
+ * Expire a key. Returns `1` if timeout was set, `0` if key does not exist.
+ */
+Redis.prototype.expire = function expire(key, seconds) {
+  return this.connection.expire(key, seconds)
+}
+
+/**
+ * @name Redis.prototype.expireat
+ *
+ * @synopsis
+ * ```coffeescript [specscript]
+ * Redis(connection).expireat(
+ *   key string,
+ *   secondsTimestamp number,
+ * ) -> status Promise<'1'|'0'>
+ * ```
+ */
+Redis.prototype.expireat = function expireat(key, secondsTimestamp) {
+  return this.connection.expireat(key, secondsTimestamp)
+}
+
+Redis.prototype.pexpire = function pexpire(key, millis) {
+  return this.connection.pexpire(key, millis)
+}
+
+Redis.prototype.pexpireat = function pexpireat(key, millisTimestamp) {
+  return this.connection.pexpireat(key, millisTimestamp)
+}
+
+Redis.prototype.persist = function persist(key) {
+  return this.connection.persist(key)
+}
+
+Redis.prototype.ttl = function ttl(key) {
+  return this.connection.ttl(key)
+}
+
+/**
+ * @name Redis.prototype.pttl
+ *
+ * @synopsis
+ * ```coffeescript [specscript]
+ * Redis(connection).pttl(key) -> timeToLiveMillis number
+ * ```
+ */
+Redis.prototype.pttl = function pttl(key) {
+  return this.connection.pttl(key)
+}
+
+/**
+ * @name Redis.prototype.type
+ *
+ * @synopsis
+ * ```coffeescript [specscript]
+ * Redis(connection).type(key string) -> Promise<'string'|'list'|'set'|'zset'|'hash'|'stream'>
+ * ```
+ */
+Redis.prototype.type = function type(key) {
+  return this.connection.type(key)
+}
+
+/**
+ * @name Redis.prototype.scan
+ *
+ * @synopsis
+ * ```coffeescript [specscript]
+ * Redis(connection).scan(
+ *   cursor string,
+ *   ('MATCH', pattern string)?,
+ *   ('COUNT', count number)?,
+ *   ('TYPE', type 'string'|'list'|'set'|'zset'|'hash'|'stream')?,
+ * ) -> [nextCursor string, elements Array]
+ * ```
+ */
+Redis.prototype.scan = function scan(cursor, ...options) {
+  return this.connection.scan(cursor, ...options)
+}
+
+/**
+ * @name Redis.prototype.del
+ *
+ * @synopsis
+ * ```coffeescript [specscript]
+ * Redis(connection).del(key string, ...moreKeys) -> numKeysDeleted Promise<number>
+ * ```
+ */
+Redis.prototype.del = function del(key, ...moreKeys) {
+  return this.connection.del(key, ...moreKeys)
+}
+
+/**
+ * @name Redis.prototype.unlink
+ *
+ * @synopsis
+ * ```coffeescript [specscript]
+ * Redis(connection).unlink(key string, ...moreKeys) -> numKeysUnlinked Promise<number>
+ * ```
+ *
+ * @description
+ * Like `del` but reclaims memory asynchronously (non-blocking).
+ */
+Redis.prototype.unlink = function unlink(key, ...moreKeys) {
+  return this.connection.unlink(key, ...moreKeys)
+}
+
+/**
+ * @name Redis.prototype.dump
+ *
+ * @synopsis
+ * ```coffeescript [specscript]
+ * Redis(connection).dump(key string) -> redisSerialized string
+ * ```
+ */
+Redis.prototype.dump = function dump(key) {
+  return this.connection.dump(key)
+}
+
+/**
+ * @name Redis.prototype.restore
+ *
+ * @synopsis
+ * ```coffeescript [specscript]
+ * Redis(connection).restore(
+ *   key string,
+ *   ttlMs number,
+ *   redisSerializedValue string,
+ *   'REPLACE'?,
+ *   'ABSTTL'?,
+ *   ('IDLETIME', seconds number)?,
+ *   ('FREQ', frequency number)?,
+ * ) -> redisSerialized string
+ * ```
+ *
+ * @description
+ * Use `0` for `ttlMs` to persist key.
+ */
+Redis.prototype.restore = function restore(
+  key, ttlMs, redisSerializedValue, ...options
+) {
+  return this.connection.restore(key, ttlMs, redisSerializedValue, ...options)
+}
+
 module.exports = Redis
--- RedisString.test.js
+++ RedisString.test.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const assert = require('assert')
 const Test = require('thunk-test')
 const Redis = require('./Redis')
@@ -5,6 +6,9 @@ const RedisString = require('./RedisString')
 
 module.exports = Test('RedisString', RedisString)
   .before(async function () {
+    try {
+      await fs.promises.rm(`${__dirname}/tmp.dump`)
+    } catch {}
     this.redis = new Redis('redis://localhost:6379/')
   })
   .beforeEach(async function () {
@@ -36,6 +40,21 @@ module.exports = Test('RedisString', RedisString)
     await testString.set('immediate-expire', 'PX', 1)
     await new Promise(resolve => setTimeout(resolve, 10))
     assert(await testString.get() == null)
+    assert.deepEqual(await this.redis.scan(0), ['0', []])
+    await testString.set('hey')
+    assert.deepEqual(await this.redis.scan(0), ['0', ['test-string']])
+    assert(await this.redis.ttl('test-string') == -1)
+    assert(await this.redis.pttl('test-string') == -1)
+    assert(await this.redis.expire('test-string', 1e6) == 1)
+    assert(await this.redis.ttl('test-string') > 0)
+    assert(await this.redis.pttl('test-string') > 0)
+    assert(await this.redis.expireat('test-string', Date.now() + 1e6) == 1)
+    assert(await this.redis.ttl('test-string') > 0)
+    assert(await this.redis.pttl('test-string') > 0)
+    assert(await this.redis.type('test-string') == 'string')
+    assert(await this.redis.del('test-string') == 1)
+    await testString.set('hey')
+    assert(await this.redis.unlink('test-string') == 1)
   })
   .after(async function () {
     await this.redis.connection.flushdb()
