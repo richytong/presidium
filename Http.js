368456233dd86ce51bf3ad25caf7d9146b9dcc6b
Richard <richytong@gmail.com>
2020-11-16 16:38:47 -0800

    more complex http

--- Http.js
+++ Http.js
@@ -1,11 +1,15 @@
 const fetch = require('node-fetch')
+const nodePath = require('path')
+const get = require('rubico/get')
+
+const pathJoin = nodePath.join
 
 /**
  * @name Http
  *
  * @synopsis
  * ```coffeescript [specscript]
- * Http(url string) -> http
+ * Http(connection string) -> http
  * ```
  *
  * @description
@@ -15,11 +19,14 @@ const fetch = require('node-fetch')
  * CONNECT method Http.prototype.connect
  * https://stackoverflow.com/questions/11697943/when-should-one-use-connect-and-get-http-methods-at-http-proxy-server/40329026
  */
-const Http = function (url) {
+const Http = function (connection, options) {
   if (this == null || this.constructor != Http) {
-    return new Http(url)
+    return new Http(connection, options)
   }
-  this.url = url
+  const url = new URL(connection)
+  this.origin = get('origin')(url)
+  this.basePath = get('pathname')(url)
+  this.httpOptions = options
   return this
 }
 
@@ -47,36 +54,67 @@ const Http = function (url) {
  *
  * [Response reference](https://developer.mozilla.org/en-US/docs/Web/API/Response).
  */
-Http.prototype.get = function (path, options) {
-  return fetch(new URL(path, this.url), options)
+Http.prototype.get = function httpGet(path, options) {
+  return fetch(new URL(pathJoin(this.basePath, path), this.origin), {
+    ...this.httpOptions,
+    ...options,
+  })
 }
 
-Http.prototype.head = function (path, options) {
-  return fetch(new URL(path, this.url), { method: 'HEAD', ...options })
+Http.prototype.head = function httpHead(path, options) {
+  return fetch(new URL(pathJoin(this.basePath, path), this.origin), {
+    ...this.httpOptions,
+    ...options,
+    method: 'HEAD',
+  })
 }
 
-Http.prototype.post = function (path, options) {
-  return fetch(new URL(path, this.url), { method: 'POST', ...options })
+Http.prototype.post = function httpPost(path, options) {
+  return fetch(new URL(pathJoin(this.basePath, path), this.origin), {
+    ...this.httpOptions,
+    ...options,
+    method: 'POST',
+  })
 }
 
-Http.prototype.put = function (path, options) {
-  return fetch(new URL(path, this.url), { method: 'PUT', ...options })
+Http.prototype.put = function httpPut(path, options) {
+  return fetch(new URL(pathJoin(this.basePath, path), this.origin), {
+    ...this.httpOptions,
+    ...options,
+    method: 'PUT',
+  })
 }
 
-Http.prototype.delete = function (path, options) {
-  return fetch(new URL(path, this.url), { method: 'DELETE', ...options })
+Http.prototype.delete = function httpDelete(path, options) {
+  return fetch(new URL(pathJoin(this.basePath, path), this.origin), {
+    ...this.httpOptions,
+    ...options,
+    method: 'DELETE',
+  })
 }
 
-Http.prototype.options = function (path, options) {
-  return fetch(new URL(path, this.url), { method: 'OPTIONS', ...options })
+Http.prototype.options = function httpOptions(path, options) {
+  return fetch(new URL(pathJoin(this.basePath, path), this.origin), {
+    ...this.httpOptions,
+    ...options,
+    method: 'OPTIONS',
+  })
 }
 
-Http.prototype.trace = function (path, options) {
-  return fetch(new URL(path, this.url), { method: 'TRACE', ...options })
+Http.prototype.trace = function httpTrace(path, options) {
+  return fetch(new URL(pathJoin(this.basePath, path), this.origin), {
+    ...this.httpOptions,
+    ...options,
+    method: 'TRACE',
+  })
 }
 
-Http.prototype.patch = function (path, options) {
-  return fetch(new URL(path, this.url), { method: 'PATCH', ...options })
+Http.prototype.patch = function httpPatch(path, options) {
+  return fetch(new URL(pathJoin(this.basePath, path), this.origin), {
+    ...this.httpOptions,
+    ...options,
+    method: 'PATCH',
+  })
 }
 
 module.exports = Http
