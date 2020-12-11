fc9708c731ed2e4774ab0985d11f553fc237c0c7
Richard <richytong@gmail.com>
2020-12-11 14:04:15 -0800

    that glorious feeling

--- DockerService.js
+++ DockerService.js
@@ -69,64 +69,6 @@ DockerService.prototype.synchronize = function dockerServiceSynchronize() {
 }
 
 /**
- * @name DockerService.prototype.create
- *
- * @synopsis
- * ```coffeescript [specscript]
- * DockerService(name).create({
- *   replicas: 1|number,
- *   restart: 'no'|'on-failure[:<max-retries>]'|'always'|'unless-stopped',
- *   restartDelay: 10e9|number, // nanoseconds to delay between restarts
- *   logDriver: 'json-file'|'syslog'|'journald'|'gelf'|'fluentd'|'awslogs'|'splunk'|'none',
- *   logDriverOptions: Object<string>,
- *   publish: Object<(hostPort string)=>(containerPort string)>,
- *   healthcheck: {
- *     test: Array<string>, // healthcheck command configuration. See description
- *     interval?: 10e9|>1e6, // nanoseconds to wait between healthchecks; 0 means inherit
- *     timeout?: 20e9|>1e6, // nanoseconds to wait before healthcheck fails
- *     retries?: 5|number, // number of retries before unhealhty
- *     startPeriod?: >=1e6, // nanoseconds to wait on container init before starting first healthcheck
- *   },
- *   mounts: Array<{
- *     source: string, // name of volume
- *     target: string, // mounted path inside container
- *     readonly: boolean,
- *   }>|Array<string>, // '<source>:<target>[:readonly]'
- *
- *   cmd: Array<string|number>, // CMD
- *   workdir: path string, // WORKDIR
- *   env: {
- *     HOME: string,
- *     HOSTNAME: string,
- *     PATH: string, // $PATH
- *     ...(moreEnvOptions Object<string>),
- *   }, // ENV; environment variables exposed to container during run time
- * }) -> {}
- * ```
- *
- * @description
- * ```javascript
- * const myService = new DockerService('my-service', {
- *   image: 'my-app:latest',
- *   env: { FOO: 'foo', BAR: 'bar' },
- *   cmd: ['npm', 'start'],
- *   replicas: 5,
- *   restart: 'on-failure',
- *   publish: { 3000: 3000 }, // hostPort: containerPort
- *   healthCmd: ['wget', '--no-verbose', '--tries=1', '--spider', 'localhost:3000'],
- *   mounts: ['my-volume:/opt/data/my-volume:readonly']
- *   logDriver: 'json-file',
- *   logDriverOptions: { 'max-file': '10', 'max-size': '100m' },
- * }) -> Promise<Object>
- * ```
- */
-DockerService.prototype.create = async function dockerServiceCreate(options) {
-  await this.ready
-  return this.docker.createService(this.name, options)
-    .then(response => response.json())
-}
-
-/**
  * @name DockerService.prototype.update
  *
  * @synopsis
