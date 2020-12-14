6b03048cb834a6e6e2d42bbec84c09cf93110aab
Richard <richytong@gmail.com>
2020-12-14 13:30:03 -0800

    clean up test

--- Elasticsearch.test.js
+++ Elasticsearch.test.js
@@ -5,10 +5,9 @@ const Elasticsearch = require('./Elasticsearch')
 module.exports = Test('Elasticsearch', Elasticsearch)
   .case('http://localhost:9200/', async function (elasticsearch) {
     const catHealth = await elasticsearch.ready
-    console.log('catHealth', catHealth)
     assert.equal(typeof elasticsearch.create, 'function')
     assert.equal(typeof elasticsearch.search, 'function')
     assert.equal(typeof elasticsearch.delete, 'function')
     assert.equal(typeof elasticsearch.count, 'function')
     assert.equal(typeof elasticsearch.cluster.health, 'function')
-  })()
+  })
