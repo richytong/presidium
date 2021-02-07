344e74bc8feaae0b9b9429f2154cd3a9fb4a502d
Richard <richytong@gmail.com>
2021-02-07 10:36:40 -0800

    0.10.2

--- internal/parseRedisConnectionString.test.js
+++ internal/parseRedisConnectionString.test.js
@@ -3,18 +3,18 @@ const parseRedisConnectionString = require('./parseRedisConnectionString')
 
 module.exports = Test(
   'parseRedisConnectionString', parseRedisConnectionString
-).case('redis://localhost:6380/100', {
+).case('redis://localhost:6380/15', {
   host: 'localhost',
   port: 6380,
-  database: 100
+  db: 15
 }).case('redis://127.0.0.1', {
   host: '127.0.0.1',
   port: 6379,
-  database: 0,
-}).case('redis://127.0.0.1/100', {
+  db: 0,
+}).case('redis://127.0.0.1/9', {
   host: '127.0.0.1',
   port: 6379,
-  database: 100,
+  database: 9,
 }).case('redis://your-domain.hash.ng.0001.use1.cache.amazonaws.com:6379/0', {
   host: 'your-domain.hash.ng.0001.use1.cache.amazonaws.com',
   port: 6379,
