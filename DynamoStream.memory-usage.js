c365aff316d95a1db8b50645e371d4550f775fd8
Richard <richytong@gmail.com>
2021-02-08 10:00:56 -0800

    lint

--- DynamoStream.memory-usage.js
+++ DynamoStream.memory-usage.js
@@ -1,12 +1,9 @@
 const DynamoTable = require('./DynamoTable')
 const DynamoStream = require('./DynamoStream')
 
-let maxHeapUsed = 0
+let maxHeapUsed = 0;
 
-const logMaxHeapUsed = function () {
-}
-
-;(async function() {
+(async function() {
   console.log('initializing dependencies')
   const table = new DynamoTable({
     name: 'local-iteration',
@@ -24,20 +21,12 @@ const logMaxHeapUsed = function () {
   (async function() {
     for await (const item of stream) {
       console.log('item', item)
-        /*
-      await table.putItem({
-        iteration: Number(item.dynamodb.NewImage.iteration.N) + 1,
-      })
-      */
     }
   })()
-  let iteration = 0
   while (true) {
     await new Promise(resolve => setTimeout(resolve, 1))
     const { heapUsed } = process.memoryUsage()
       maxHeapUsed = Math.max(maxHeapUsed, heapUsed)
     console.log('maxHeapUsed (MiB)', heapUsed, maxHeapUsed / 1024 / 1024)
-    // await table.putItem({ iteration })
   }
-  // maxHeapUsed goes up every ~5mins
 })()
