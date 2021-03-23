b6e431426acb82ffbae0993d5080d6da1fe4f8ab
Richard <richytong@gmail.com>
2021-03-23 10:34:40 -0700

    more query entrypoints

--- ElasticsearchIndex.js
+++ ElasticsearchIndex.js
@@ -183,6 +183,60 @@ ElasticsearchIndex.prototype.getDocument = function getDocument(
 }
 
 /**
+ * @name ElasticsearchIndex.prototype.query
+ *
+ * @synopsis
+ * ```coffeescript [specscript]
+ * new ElasticsearchIndex(options).query(
+ *   dsl Object,
+ *   options? {
+ *     size: number,
+ *     from: number,
+ *     to: number,
+ *   },
+ * )
+ * ```
+ */
+ElasticsearchIndex.prototype.query = function query(dsl, options) {
+  return this.http.post('/_search', {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: dsl,
+      ...pick(['size', 'from', 'to'])(options)
+    }),
+  })
+}
+
+/**
+ * @name ElasticsearchIndex.prototype.term
+ *
+ * @synopsis
+ * ```coffeescript [specscript]
+ * newn Elasticsearch(options).term(
+ *   termDSL {
+ *     [field string]: {
+ *       value: string,
+ *       boost?: number,
+ *     },
+ *   },
+ *   options? {
+ *     size: number,
+ *     from: number,
+ *     to: number,
+ *   },
+ * ) -> Promise<HttpResponse>
+ * ```
+ *
+ * @description
+ * https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-term-query.html
+ */
+ElasticsearchIndex.prototype.term = function term(termDSL, options) {
+  return this.query({ term: termDSL }, options)
+}
+
+/**
  * @name ElasticsearchIndex.prototype.match
  *
  * @synopsis
@@ -197,11 +251,10 @@ ElasticsearchIndex.prototype.getDocument = function getDocument(
  *     from: number,
  *     to: number,
  *   },
- * ) -> ElasticsearchIndex
+ * ) -> Promise<HttpResponse>
  * ```
  *
  * @description
- * match query DSL reference:
  * https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-match-query.html
  *
  * ```javascript
@@ -218,17 +271,147 @@ ElasticsearchIndex.prototype.getDocument = function getDocument(
  */
 
 ElasticsearchIndex.prototype.match = function match(matchDSL, options) {
-  return this.http.post('/_search', {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      query: {
-        match: matchDSL,
-      },
-      ...pick(['size', 'from', 'to'])(options)
-    }),
-  })
+  return this.query({ match: matchDSL }, options)
+}
+
+/**
+ * @name ElasticsearchIndex.prototype.bool
+ *
+ * @synopsis
+ * ```coffeescript [specscript]
+ * new ElasticsearchIndex(options).bool(
+ *   boolDSL {
+ *     must?: Array|DSL, // clause (query) must appear, contributes to score
+ *     filter?: Array|DSL, // clause (query) must appear, does not contribute to score
+ *     should?: Array|DSL, // clause (query) _should_ appear, contributes to score
+ *     must_not?: Array|DSL, // clause (query) must not appear, ignores scoring
+ *   },
+ *   options? {
+ *     size: number,
+ *     from: number,
+ *     to: number,
+ *   },
+ * ) -> Promise<HttpResponse>
+ * ```
+ *
+ * @description
+ * https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-bool-query.html
+ */
+
+ElasticsearchIndex.prototype.bool = function bool(boolDSL, options) {
+  return this.query({ bool: boolDSL }, options)
+}
+
+/**
+ * @name ElasticsearchIndex.prototype.disMax
+ *
+ * @synopsis
+ * ```coffeescript [specscript]
+ * new ElasticsearchIndex(options).disMax(
+ *   disMaxDSL {
+ *     queries: Array<DSL>,
+ *     tie_breaker?: number, // [0, 1.0] used to increase relevance scores of documents matching multiple query clauses
+ *   },
+ *   options? {
+ *     size: number,
+ *     from: number,
+ *     to: number,
+ *   },
+ * ) -> Promise<HttpResponse>
+ * ```
+ *
+ * @description
+ * https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-dis-max-query.html
+ */
+ElasticsearchIndex.prototype.disMax = function disMax(disMaxDSL, options) {
+  return this.query({ dis_max: disMaxDSL }, options)
+}
+
+/**
+ * @name ElasticsearchIndex.prototype.functionScore
+ *
+ * @synopsis
+ * ```coffeescript [specscript]
+ * new ElasticsearchIndex(options).functionScore(
+ *   functionScoreDSL {
+ *     query: DSL,
+ *     boost?: number, // boost for the whole query
+ *     functions?: Array<{
+ *       filter: DSL,
+ *       weight?: number, // multiplies score in final computed score for a document
+ *       script_score?: {
+ *         script: {
+ *           source: string, // Elasticsearch script expression, e.g. `Math.log(2 + doc['my-int'].value)`
+ *           params?: Object,
+ *         },
+ *       },
+ *       random_score?: {}|{ // provide seed and field for reproducible scores
+ *         seed: number,
+ *         field: string, // e.g. '_seq_no' (builtin)
+ *       },
+ *       field_value_factor?: {
+ *         field: string, // field in document e.g. 'my-field'
+ *         factor?: number, // multiply field value
+ *         modifier?: // modify field value, default 'none'
+ *           'none' // no multiplier
+ *           |'log' // common log
+ *           |'log1p' // add 1 then common log
+ *           |'log2p' // add 2 then common log
+ *           |'ln' // natural log
+ *           |'ln1p' // add 1 then natural log
+ *           |'ln2p' // add 2 then common log
+ *           |'square' // ^2
+ *           |'sqrt' // square root
+ *           |'reciprocal', // 1/x
+ *         missing: number, // value to use in case of missing field
+ *       },
+ *       ['linear'|'exp'|'gauss']: { // decay function to use
+ *         [field string]: { // Note: only numeric, date, and geo_point are supported
+ *           origin: number|string, // origin for calculating distance. Must be given as a number for numeric field, date for date fields, and geo point for geo fields
+ *           scale: number|string, // distance from origin + offset at which computed score == `decay` parameter.
+ *                                 // geo: default unit meters [m] (e.g. 10m)
+ *                                 // date: default milliseconds [ms] (e.g. 1h, 10d)
+ *           offset: number|string, // only compute decay function for documents with a distance greater than this parameter, default 0
+ *           decay: number, // defines how documents are scored at the distance given in `scale`. Default 0.5
+ *         },
+ *         multi_value_mode: // for multi-value fields, determine which value to use for calculating distance
+ *           'min' // use minimum computed distance
+ *           |'max' // use maximum computed distance
+ *           |'avg' // use average of computed distances
+ *           |'sum' // use sum of all distances
+ *       },
+ *     }>
+ *     max_boost?: number, // maximum a document's score can be boosted
+ *     score_mode?: // specify how individual computed scores are combined
+ *       'multiply' // scores are multiplied (default)
+ *       |'sum' // add scores
+ *       |'avg' // average scores
+ *       |'first' // first function that has a matching filter is applied
+ *       |'max' // max of scores
+ *       |'min', // min of scores
+ *     boost_mode?: // specify how final computed score is combined with score of the query
+ *       'multiply' // query score and function score are multiplied (default)
+ *       |'replace' // only function score is used, query score is ignored
+ *       |'sum' // add query score and function score
+ *       |'avg' // add query score and function score
+ *       |'max' // maximum of query score and function score
+ *       |'min', // minimum of query score and function score
+ *   },
+ *   options? {
+ *     size: number,
+ *     from: number,
+ *     to: number,
+ *   }
+ * ) -> Promise<HttpResponse>
+ * ```
+ *
+ * @description
+ * https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-function-score-query.html
+ */
+ElasticsearchIndex.prototype.functionScore = function functionScore(
+  functionScoreDSL, options,
+) {
+  return this.query({ function_score: functionScoreDSL }, options)
 }
 
 module.exports = ElasticsearchIndex
--- ElasticsearchIndex.test.js
+++ ElasticsearchIndex.test.js
@@ -25,24 +25,31 @@ const test = Test('ElasticsearchIndex', async function () {
     node: 'http://localhost:9200/',
     index: 'test-index',
     mappings: {
+      slug: { type: 'keyword' },
       text: { type: 'text' },
+      rating: { type: 'integer' },
     },
   })
-  await testIndex.delete()
   await testIndex.ready
 
   await testIndex.indexDocument({
+    slug: 'hello-there',
     text: 'hello there',
+    rating: 9,
   }, {
     refresh: 'true',
   })
   await testIndex.indexDocument({
+    slug: 'hello-world',
     text: 'hello world',
+    rating: 8,
   }, {
     refresh: 'wait_for',
   })
   this.goodbyeWorldElasticsearchId = await testIndex.indexDocument({
+    slug: 'goodbye-world',
     text: 'goodbye world',
+    rating: 7,
   }, {
     refresh: 'false',
   }).then(pipe([
@@ -50,6 +57,18 @@ const test = Test('ElasticsearchIndex', async function () {
     get('_id'),
   ]))
   await testIndex.refresh()
+
+  {
+    const response = await testIndex.term({
+      slug: 'hello-world',
+    })
+    assert.strictEqual(response.status, 200)
+    const data = await response.json()
+    assert.strictEqual(data.hits.total.value, 1)
+    assert.strictEqual(data.hits.hits.length, 1)
+    const doc = data.hits.hits[0]._source
+    assert.strictEqual(doc.slug, 'hello-world')
+  }
 
   {
     const response = await testIndex.match({
@@ -61,6 +80,53 @@ const test = Test('ElasticsearchIndex', async function () {
     assert.strictEqual(data.hits.hits.length, 2)
     const texts = data.hits.hits.map(get('_source.text'))
     assert(texts.every(includes('hello')))
+  }
+
+  {
+    const response = await testIndex.bool({
+      must_not: [
+        { term: { slug: 'goodbye-world' } },
+      ],
+    }, { size: 100 })
+    assert.strictEqual(response.status, 200)
+    const data = await response.json()
+    assert.strictEqual(data.hits.total.value, 2)
+    assert.strictEqual(data.hits.hits.length, 2)
+    const texts = data.hits.hits.map(get('_source.text'))
+    assert(texts.every(includes('hello')))
+  }
+
+  {
+    const response = await testIndex.disMax({
+      queries: [
+        { term: { slug: 'hello-there' } },
+        { term: { slug: 'hello-world' } },
+      ],
+      tie_breaker: 0,
+    }, { size: 100 })
+    assert.strictEqual(response.status, 200)
+    const data = await response.json()
+    assert.strictEqual(data.hits.total.value, 2)
+    assert.strictEqual(data.hits.hits.length, 2)
+    const texts = data.hits.hits.map(get('_source.text'))
+    assert(texts.every(includes('hello')))
+  }
+
+  {
+    const response = await testIndex.functionScore({
+      field_value_factor: {
+        field: 'rating',
+        factor: 1.2,
+        modifier: 'sqrt',
+        missing: 1,
+      },
+    })
+    assert.strictEqual(response.status, 200)
+    const data = await response.json()
+    assert.strictEqual(data.hits.total.value, 3)
+    assert.strictEqual(data.hits.hits.length, 3)
+    const ratings = data.hits.hits.map(get('_source.rating'))
+    assert.deepEqual(ratings, [...ratings].sort((a, b) => b - a)) // desc
   }
 
   {
