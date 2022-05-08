c60ff574877ad1fd0e749f3962277f4e8d57b718
Richard <richytong@gmail.com>
2022-05-07 20:00:01 -0700

    ECR getAuthorizationToken

---
+++ ECR.test.js
@@ -0,0 +1,31 @@
+const Test = require('thunk-test')
+const ECR = require('./ECR')
+const AwsCredentials = require('./internal/AwsCredentials')
+
+const test = new Test('ECR', async function () {
+  const awsCreds = await AwsCredentials('default').catch(error => {
+    if (error.code == 'ENOENT') {
+      const accessKeyId = process.env.AWS_ACCESS_KEY_ID
+      const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
+      if (accessKeyId == null || secretAccessKey == null) {
+        throw new Error('No AWS credential file or environment variables')
+      }
+      return { accessKeyId, secretAccessKey }
+    }
+    throw error
+  })
+  awsCreds.region = 'us-west-1'
+
+  const ecr = new ECR({
+    ...awsCreds,
+  })
+
+  const data = await ecr.getAuthorizationToken()
+  assert.equal(typeof data.authorizationToken, 'string')
+}).case()
+
+if (process.argv[1] == __filename) {
+  test()
+}
+
+module.exports = test
