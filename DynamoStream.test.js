9d42d78671aa9528753aed1ca184ea65ee1a7f74
Richard <richytong@gmail.com>
2022-11-11 10:33:01 -0800

    improve DynamoStream coverage

--- DynamoStream.test.js
+++ DynamoStream.test.js
@@ -38,6 +38,7 @@ const test = Test('DynamoStream', DynamoStream)
   table: 'my-table',
   endpoint: 'http://localhost:8000',
   shardIteratorType: 'TRIM_HORIZON',
+  shardUpdatePeriod: 1000,
 }, async function (myStream) {
   await myStream.ready
 
@@ -83,7 +84,7 @@ const test = Test('DynamoStream', DynamoStream)
   endpoint: 'http://localhost:8000',
   getRecordsLimit: 1,
   getRecordsInterval: 1000,
-  shardUpdatePeriod: 5000,
+  shardUpdatePeriod: 1000,
   shardIteratorType: 'TRIM_HORIZON',
 }, async function (myStream) {
   await myStream.ready
@@ -129,6 +130,7 @@ const test = Test('DynamoStream', DynamoStream)
   table: 'my-table',
   endpoint: 'http://localhost:8000',
   getRecordsLimit: 1,
+  shardUpdatePeriod: 1000,
   shardIteratorType: 'TRIM_HORIZON',
 }, async function (myStream) {
   await myStream.ready
@@ -183,8 +185,85 @@ const test = Test('DynamoStream', DynamoStream)
   listStreamsLimit: 1,
   shardIteratorType: 'TRIM_HORIZON',
   debug: true,
+  shardUpdatePeriod: 1000,
 }, async function (myStream) {
   await myStream.ready
+
+  const table = this.table
+  await table.putItem({
+    id: '1',
+    status: 'waitlist',
+    createTime: 1000,
+    name: 'George',
+  })
+  await table.putItem({
+    id: '2',
+    status: 'waitlist',
+    createTime: 1001,
+    name: 'geo',
+  })
+  await table.putItem({
+    id: '3',
+    status: 'waitlist',
+    createTime: 1002,
+    name: 'john',
+  })
+  await table.putItem({
+    id: '4',
+    status: 'approved',
+    createTime: 1003,
+    name: 'sally',
+  })
+  await table.putItem({
+    id: '5',
+    status: 'approved',
+    createTime: 1004,
+    name: 'sally',
+  })
+
+  const first5 = await asyncIterableTake(5)(myStream)
+  assert.strictEqual(first5.length, 5)
+  myStream.close()
+})
+
+.case({
+  table: 'my-table',
+  endpoint: 'http://localhost:8000',
+  listStreamsLimit: 1,
+  shardIteratorType: 'TRIM_HORIZON',
+  debug: true,
+  shardUpdatePeriod: 1000,
+}, async function (myStream) {
+  await myStream.ready
+
+  const originalClient = myStream.client
+  const alternatingErrorClient = {
+    listStreams(options) {
+      return originalClient.listStreams(options)
+    },
+    describeStream(options) {
+      return originalClient.describeStream(options)
+    },
+    getShardIterator(options) {
+      return originalClient.getShardIterator(options)
+    },
+    getRecordsCount: 0,
+    getRecords(options) {
+      return {
+        async promise() {
+          if (alternatingErrorClient.getRecordsCount % 2 == 0) {
+            const error = new Error('stub error')
+            error.retryable = true
+            alternatingErrorClient.getRecordsCount += 1
+            throw error
+          }
+          alternatingErrorClient.getRecordsCount += 1
+          return originalClient.getRecords(options).promise()
+        },
+      }
+    },
+  }
+  myStream.client = alternatingErrorClient
 
   const table = this.table
   await table.putItem({
