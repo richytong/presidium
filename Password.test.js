4772739dcbdbd19a75f4e1b48deb70c5a2f6649f
Richard <richytong@gmail.com>
2022-12-11 19:15:42 -0800

    test password throwing

--- Password.test.js
+++ Password.test.js
@@ -5,6 +5,10 @@ const Password = require('./Password')
 const test = new Test('Password', async function () {
   const plaintext = 'MyPassword!123'
   const hashed = await Password.hash(plaintext)
+  await assert.rejects(
+    Password.verify('incorrect', hashed),
+    new Error('Invalid password'),
+  )
   await Password.verify(plaintext, hashed) // should not throw
 }).case()
 
