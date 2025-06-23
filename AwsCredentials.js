b8c059163797103b086ea9730ba472340ccd300a
Richard <richytong@gmail.com>
2025-06-23 12:37:07 -0700

    AwsCredentials finds region from .aws/config

--- AwsCredentials.js
+++ AwsCredentials.js
@@ -9,7 +9,10 @@ const pathResolve = require('./internal/pathResolve')
  * @synopsis
  * ```coffeescript [specscript]
  * AwsCredentials(profile string, options? {
+ *   credentialsFileDirname?: string
  *   credentialsFilename?: string,
+ *   configFileDirname?: string
+ *   configFilename?: string,
  * }) -> awsCreds {
  *   accessKeyId: string,
  *   secretAccessKey: string,
@@ -18,15 +21,32 @@ const pathResolve = require('./internal/pathResolve')
  */
 
 const AwsCredentials = async function (profile, options = {}) {
-  if (process.env.AWS_ACCESS_KEY_ID && process.env.AWS_SECRET_ACCESS_KEY) {
+  if (
+    process.env.AWS_ACCESS_KEY_ID
+    && process.env.AWS_SECRET_ACCESS_KEY
+    && process.env.AWS_REGION
+  ) {
+    const awsCreds = {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      region: process.env.AWS_REGION,
+    }
+    return awsCreds
+  }
+
+  if (
+    process.env.AWS_ACCESS_KEY_ID
+    && process.env.AWS_SECRET_ACCESS_KEY
+  ) {
     const awsCreds = {
       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     }
-    if (process.env.AWS_REGION) {
-      awsCreds.region = process.env.AWS_REGION
-    }
     return awsCreds
+  }
+
+  if (profile == null) {
+    profile = 'default'
   }
 
   const credentialsFileDirname = options.credentialsFileDirname ?? '.aws'
@@ -50,14 +70,37 @@ const AwsCredentials = async function (profile, options = {}) {
 
   const accessKeyId = lines.find(
     (line, index) => index > startingLineNumber
-    && line.startsWith('aws_access_key_id')
+      && line.startsWith('aws_access_key_id')
   ).split(' = ')[1]
   const secretAccessKey = lines.find(
     (line, index) => index > startingLineNumber
-    && line.startsWith('aws_secret_access_key')
+      && line.startsWith('aws_secret_access_key')
   ).split(' = ')[1]
 
-  return { accessKeyId, secretAccessKey }
+  const configFileDirname = options.configFileDirname ?? '.aws'
+  const configFilename = options.configFilename ?? 'config'
+  let configFileDir = pathResolve(process.cwd())
+  let lines2 = ''
+  while (configFileDir != '/') {
+    const configFilePath = pathResolve(configFileDir, `${configFileDirname}/${configFilename}`)
+    if (fs.existsSync(configFilePath)) {
+      lines2 = await (
+        fs.promises.readFile(configFilePath)
+        .then(buffer => `${buffer}`.split('\n'))
+      )
+      break
+    }
+    configFileDir = pathResolve(configFileDir, '..')
+  }
+
+  const startingLineNumber2 = lines2.findIndex(line => line == `[${profile}]`)
+
+  const region = lines2.find(
+    (line, index) => index > startingLineNumber2
+      && line.startsWith('region')
+  ).split(' = ')[1]
+
+  return { accessKeyId, secretAccessKey, region }
 }
 
 module.exports = AwsCredentials
--- AwsCredentials.test.js
+++ AwsCredentials.test.js
@@ -6,6 +6,8 @@ const AwsCredentials = require('./AwsCredentials')
 const test = new Test('AwsCredentials', async function () {
   const credentialsFilename = 'credentials-test'
   const credentialsFileDirname = '.aws-test'
+  const configFilename = 'config-test'
+  const configFileDirname = credentialsFileDirname
 
   try {
     await fs.promises.mkdir(`${__dirname}/../${credentialsFileDirname}`)
@@ -16,35 +18,86 @@ const test = new Test('AwsCredentials', async function () {
 
   await fs.promises.writeFile(`${__dirname}/../${credentialsFileDirname}/${credentialsFilename}`, `
 [default]
-aws_access_key_id = X
-aws_secret_access_key = X
+aws_access_key_id = X1
+aws_secret_access_key = X2
 
 [presidium]
 aws_access_key_id = AAA
 aws_secret_access_key = BBB
   `.trim())
 
+  await fs.promises.writeFile(`${__dirname}/../${configFileDirname}/${configFilename}`, `
+[default]
+region = us-east-default
+
+[presidium]
+region = us-east-presidium
+  `.trim())
+
   {
-    const awsCreds = await AwsCredentials('presidium', { credentialsFileDirname, credentialsFilename })
+    const awsCreds = await AwsCredentials('presidium', {
+      credentialsFileDirname,
+      credentialsFilename,
+      configFileDirname,
+      configFilename,
+    })
+    assert.equal(awsCreds.accessKeyId, 'AAA')
     assert.equal(awsCreds.secretAccessKey, 'BBB')
+    assert.equal(awsCreds.region, 'us-east-presidium')
+  }
+
+  {
+    const awsCreds = await AwsCredentials('default', {
+      credentialsFileDirname,
+      credentialsFilename,
+      configFileDirname,
+      configFilename,
+    })
+    assert.equal(awsCreds.accessKeyId, 'X1')
+    assert.equal(awsCreds.secretAccessKey, 'X2')
+    assert.equal(awsCreds.region, 'us-east-default')
+  }
+
+  {
+    const awsCreds = await AwsCredentials(undefined, {
+      credentialsFileDirname,
+      credentialsFilename,
+      configFileDirname,
+      configFilename,
+    })
+    assert.equal(awsCreds.accessKeyId, 'X1')
+    assert.equal(awsCreds.secretAccessKey, 'X2')
+    assert.equal(awsCreds.region, 'us-east-default')
   }
 
   {
     process.env.AWS_ACCESS_KEY_ID = 'AAAA'
     process.env.AWS_SECRET_ACCESS_KEY = 'BBBB'
-    const awsCreds = await AwsCredentials('presidium', { credentialsFileDirname, credentialsFilename })
+    process.env.AWS_REGION = 'CCCC'
+    const awsCreds = await AwsCredentials('presidium', {
+      credentialsFileDirname,
+      credentialsFilename,
+      configFileDirname,
+      configFilename,
+    })
     assert.equal(awsCreds.accessKeyId, 'AAAA')
     assert.equal(awsCreds.secretAccessKey, 'BBBB')
+    assert.equal(awsCreds.region, 'CCCC')
   }
 
   {
     process.env.AWS_ACCESS_KEY_ID = 'AAAA'
     process.env.AWS_SECRET_ACCESS_KEY = 'BBBB'
-    process.env.AWS_REGION = 'us-east-2'
-    const awsCreds = await AwsCredentials('presidium', { credentialsFileDirname, credentialsFilename })
+    delete process.env.AWS_REGION
+    const awsCreds = await AwsCredentials('presidium', {
+      credentialsFileDirname,
+      credentialsFilename,
+      configFileDirname,
+      configFilename,
+    })
     assert.equal(awsCreds.accessKeyId, 'AAAA')
     assert.equal(awsCreds.secretAccessKey, 'BBBB')
-    assert.equal(awsCreds.region, 'us-east-2')
+    assert.equal(awsCreds.region, undefined)
   }
 
   await fs.promises.rm(`${__dirname}/../${credentialsFileDirname}`, { recursive: true })
