32c26494963a1a7e52fc2dece257bc36ed347963
Richard <richytong@gmail.com>
2025-06-28 10:30:00 -0700

    Http use node http and https

--- Http.js
+++ Http.js
@@ -1,104 +1,225 @@
-const fetch = require('node-fetch')
-const nodePath = require('path')
-const get = require('rubico/get')
-const curry = require('rubico/curry')
-
-const pathJoin = nodePath.join
+const http = require('http')
+const https = require('https')
+const path = require('path')
 
 /**
  * @name Http
- *
- * @synopsis
- * ```coffeescript [specscript]
- * Http(url string|URL) -> http
- * ```
- *
- * @description
- * The Hypertext Transfer Protocol. [wikipedia](https://en.wikipedia.org/wiki/Hypertext_Transfer_Protocol)
- *
- * @todo
- * CONNECT method Http.prototype.connect
- * https://stackoverflow.com/questions/11697943/when-should-one-use-connect-and-get-http-methods-at-http-proxy-server/40329026
  */
-const Http = function (url, httpOptions) {
-  if (this == null || this.constructor != Http) {
-    return new Http(url, httpOptions)
+class Http {
+  constructor(baseUrl) {
+    this.baseUrl = baseUrl
+    this.client = baseUrl.includes('https') ? https : http
   }
-  this.url = new URL(url)
-  this.httpOptions = httpOptions
-  return this
-}
 
-/**
- * @name Http.prototype.get
- *
- * @synopsis
- * ```coffeescript [specscript]
- * Http(url).get(path string, options {
- *   body: string,
- *   headers: Object<string>,
- * }) -> Promise<Response>
- * ```
- *
- * @description
- * Makes an http GET request. [All options](https://github.com/node-fetch/node-fetch#options) from node-fetch.
- *
- * ```javascript
- * Http('https://google.com/').get('/')
- *   .then(res => res.text())
- *   .then(console.log)
- * ```
- *
- * [http method reference](https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods)
- *
- * [Response reference](https://developer.mozilla.org/en-US/docs/Web/API/Response).
- */
-Http.prototype.get = function httpGet(path, options) {
-  const url = new URL(this.url),
-    pathUrl = new URL(`http://throwaway/${path}`)
-  url.pathname = pathJoin(url.pathname, pathUrl.pathname)
-  url.search = pathUrl.search
-  return fetch(url, {
-    ...this.httpOptions,
-    ...options,
-  })
-}
+  /**
+   * @name request
+   *
+   * @synopsis
+   * ```coffeescript [specscript]
+   * request(options {
+   *   hostname: string,
+   *   port: number,
+   *   path: string,
+   *   method: string,
+   *   headers: object,
+   *   body: Buffer|TypedArray|string,
+   * }) -> Promise<response http.ServerResponse>
+   * ```
+   */
+  request(options) {
+    const { body, ...requestOptions } = options
+    return new Promise((resolve, reject) => {
+      const request = this.client.request(requestOptions, response => {
+        response.status = response.statusCode
 
-// (method string, path string, options object) => Promise<HttpResponse>
-const httpRequest = function httpRequest(method, path, options) {
-  const url = new URL(this.url),
-    pathUrl = new URL(`http://throwaway/${path}`)
-  url.pathname = pathJoin(url.pathname, pathUrl.pathname)
-  url.search = pathUrl.search
-  return fetch(url, { ...this.httpOptions, ...options, method })
-}
+        const chunks = []
 
-Http.prototype.head = function head(path, options) {
-  return httpRequest.call(this, 'HEAD', path, options)
-}
+        response.bytes = () => new Promise((resolve2, reject2) => {
+          if (response.ended) {
+            resolve2(new Uint8Array(Buffer.concat(chunks)))
+          } else {
+            response.on('data', chunk => {
+              chunks.push(chunk)
+            })
+            response.on('end', () => {
+              response.ended = true
+              resolve2(new Uint8Array(Buffer.concat(chunks)))
+            })
+            response.on('error', reject2)
+          }
+        })
 
-Http.prototype.post = function post(path, options) {
-  return httpRequest.call(this, 'POST', path, options)
-}
+        response.text = () => new Promise((resolve2, reject2) => {
+          if (response.ended) {
+            resolve2(chunks.map(chunk => chunk.toString('utf8')).join(''))
+          } else {
+            response.on('data', chunk => {
+              chunks.push(chunk)
+            })
+            response.on('end', () => {
+              response.ended = true
+              resolve2(chunks.map(chunk => chunk.toString('utf8')).join(''))
+            })
+            response.on('error', reject2)
+          }
+        })
 
-Http.prototype.put = function put(path, options) {
-  return httpRequest.call(this, 'PUT', path, options)
-}
+        response.json = () => new Promise((resolve2, reject2) => {
+          if (response.ended) {
+            try {
+              resolve2(JSON.parse(chunks.map(chunk => chunk.toString('utf8')).join('')))
+            } catch (error) {
+              reject2(error)
+            }
+          } else {
+            response.on('data', chunk => {
+              chunks.push(chunk)
+            })
+            response.on('end', () => {
+              response.ended = true
+              try {
+                resolve2(JSON.parse(chunks.map(chunk => chunk.toString('utf8')).join('')))
+              } catch (error) {
+                reject2(error)
+              }
+            })
+            response.on('error', reject2)
+          }
+        })
 
-Http.prototype.delete = function del(path, options) {
-  return httpRequest.call(this, 'DELETE', path, options)
-}
+        resolve(response)
+      })
 
-Http.prototype.options = function opts(path, options) {
-  return httpRequest.call(this, 'OPTIONS', path, options)
-}
+      request.on('error', reject)
+      request.end(body)
+    })
+  }
 
-Http.prototype.trace = function trace(path, options) {
-  return httpRequest.call(this, 'TRACE', path, options)
-}
+  get(relativeUrl, options = {}) {
+    console.log('get', relativeUrl, options)
+    const url = path.join(this.baseUrl, relativeUrl)
+    const parsedUrl = new URL(url)
+    const requestOptions = {
+      hostname: parsedUrl.hostname,
+      port: parsedUrl.port,
+      path: parsedUrl.pathname,
+      method: 'GET',
+      headers: options.headers ?? {},
+      body: options.body,
+    }
+    return this.request(requestOptions)
+  }
 
-Http.prototype.patch = function patch(path, options) {
-  return httpRequest.call(this, 'PATCH', path, options)
+  head(relativeUrl, options = {}) {
+    const url = path.join(this.baseUrl, relativeUrl)
+    const parsedUrl = new URL(url)
+    const requestOptions = {
+      hostname: parsedUrl.hostname,
+      port: parsedUrl.port,
+      path: parsedUrl.pathname,
+      method: 'HEAD',
+      headers: options.headers ?? {},
+      body: options.body,
+    }
+    return this.request(requestOptions)
+  }
+
+  post(relativeUrl, options = {}) {
+    const url = path.join(this.baseUrl, relativeUrl)
+    const parsedUrl = new URL(url)
+    const requestOptions = {
+      hostname: parsedUrl.hostname,
+      port: parsedUrl.port,
+      path: parsedUrl.pathname,
+      method: 'POST',
+      headers: options.headers ?? {},
+      body: options.body,
+    }
+    return this.request(requestOptions)
+  }
+
+  put(relativeUrl, options = {}) {
+    const url = path.join(this.baseUrl, relativeUrl)
+    const parsedUrl = new URL(url)
+    const requestOptions = {
+      hostname: parsedUrl.hostname,
+      port: parsedUrl.port,
+      path: parsedUrl.pathname,
+      method: 'PUT',
+      headers: options.headers ?? {},
+      body: options.body,
+    }
+    return this.request(requestOptions)
+  }
+
+  patch(relativeUrl, options = {}) {
+    const url = path.join(this.baseUrl, relativeUrl)
+    const parsedUrl = new URL(url)
+    const requestOptions = {
+      hostname: parsedUrl.hostname,
+      port: parsedUrl.port,
+      path: parsedUrl.pathname,
+      method: 'PATCH',
+      headers: options.headers ?? {},
+      body: options.body,
+    }
+    return this.request(requestOptions)
+  }
+
+  delete(relativeUrl, options = {}) {
+    const url = path.join(this.baseUrl, relativeUrl)
+    const parsedUrl = new URL(url)
+    const requestOptions = {
+      hostname: parsedUrl.hostname,
+      port: parsedUrl.port,
+      path: parsedUrl.pathname,
+      method: 'DELETE',
+      headers: options.headers ?? {},
+      body: options.body
+    }
+    return this.request(requestOptions)
+  }
+
+  connect(options = {}) {
+    const parsedUrl = new URL(this.baseUrl)
+    const requestOptions = {
+      host: parsedUrl.hostname,
+      port: Number(parsedUrl.port),
+      method: 'CONNECT',
+      path: options.path,
+    }
+    const request = this.client.request(requestOptions)
+    request.end(options.body)
+    return request
+  }
+
+  options(relativeUrl, options2 = {}) {
+    const url = path.join(this.baseUrl, relativeUrl)
+    const parsedUrl = new URL(url)
+    const requestOptions = {
+      hostname: parsedUrl.hostname,
+      port: parsedUrl.port,
+      path: parsedUrl.pathname,
+      method: 'OPTIONS',
+      headers: options2.headers ?? {},
+      body: options2.body,
+    }
+    return this.request(requestOptions)
+  }
+
+  trace(relativeUrl, options = {}) {
+    const url = path.join(this.baseUrl, relativeUrl)
+    const parsedUrl = new URL(url)
+    const requestOptions = {
+      hostname: parsedUrl.hostname,
+      port: parsedUrl.port,
+      path: parsedUrl.pathname,
+      method: 'TRACE',
+      headers: options.headers ?? {},
+      body: options.body,
+    }
+    return this.request(requestOptions)
+  }
 }
 
 module.exports = Http
--- Http.test.js
+++ Http.test.js
@@ -1,86 +1,217 @@
 const assert = require('assert')
 const Test = require('thunk-test')
-const HttpServer = require('./HttpServer')
+const http = require('http')
+const { connect } = require('net')
 const Http = require('./Http')
+const RequestBuffer = require('./RequestBuffer')
 
-module.exports = Test('Http', Http)
-  .before(function () {
-    this.httpServer = HttpServer((request, response) => {
-      response.writeHead(200, { 'Content-Type': 'application/json' })
-      switch (request.method) {
-        case 'GET': case 'OPTIONS': case 'TRACE': case 'HEAD': {
-          response.write(JSON.stringify({ greeting: 'Hello World' }))
-          response.end()
-          break
-        } case 'POST': {
-          request.pipe(response)
-          break
-        } case 'PUT': {
-          request.pipe(response)
-          break
-        } case 'PATCH': {
-          request.pipe(response)
-          break
-        } case 'DELETE': {
-          request.pipe(response)
-          break
-        } default: {
-          throw new Error(`unknown request method ${request.method}`)
-        }
-      }
-    }).listen(3000)
+const test1 = new Test('Http GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, and TRACE', async () => {
+  const server = http.createServer(async (request, response) => {
+    if (request.url == '/invalid-json') {
+      response.write('{"greeting":"Hello Wor')
+      response.end()
+    } else if (request.url == '/echo') {
+      const buffer = await RequestBuffer(request)
+      const requestBodyString = buffer.toString('utf8')
+      const requestBodyJSON = requestBodyString.length == 0 ? {} : JSON.parse(requestBodyString)
+      delete request.headers['content-length']
+      response.writeHead(200, {
+        'Content-Type': 'application/json',
+        ...request.headers,
+      })
+      response.end(JSON.stringify({ greeting: 'Hello World', ...requestBodyJSON }))
+    } else {
+      response.writeHead(404, {
+        'Content-Type': 'text/plain',
+      })
+      response.end('Not Found')
+    }
   })
-  .case('http://localhost:3000/', async helloHttp => {
-    const response = await helloHttp.get('/')
-    assert(response.headers.get('content-type') == 'application/json')
+
+  server.listen(3000, () => {
+    console.log('server listening on port 3000')
+  })
+
+  const _http = new Http('http://localhost:3000/')
+
+  {
+    const response = await _http.get('/echo')
+    assert.equal(response.status, 200)
+    const data = await response.json()
+    assert.equal(response.headers['content-type'], 'application/json')
+    assert.equal(Buffer.from(await response.bytes()).toString('utf8'), '{"greeting":"Hello World"}')
+    assert.equal(Buffer.from(await response.bytes()).toString('utf8'), '{"greeting":"Hello World"}')
+    assert.equal(await response.text(), '{"greeting":"Hello World"}')
+    assert.equal(await response.text(), '{"greeting":"Hello World"}')
     assert.deepEqual(await response.json(), { greeting: 'Hello World' })
-  })
-  .case('http://localhost:3000/', async helloHttp => {
-    const response = await helloHttp.head('/')
-    assert.strictEqual(response.status, 200)
-  })
-  .case('http://localhost:3000/', async helloHttp => {
-    const response = await helloHttp.post('/', {
-      body: JSON.stringify({ a: 1 }),
-      headers: { 'Content-Type': 'application/json' },
-    })
-    assert(response.headers.get('content-type') == 'application/json')
-    assert.deepEqual(await response.json(), { a: 1 })
-  })
-  .case('http://localhost:3000/', async helloHttp => {
-    const response = await helloHttp.put('/', {
-      body: JSON.stringify({ a: 1 }),
-      headers: { 'Content-Type': 'application/json' },
-    })
-    assert(response.headers.get('content-type') == 'application/json')
-    assert.deepEqual(await response.json(), { a: 1 })
-  })
-  .case('http://localhost:3000/', async helloHttp => {
-    const response = await helloHttp.delete('/', {
-      body: JSON.stringify({ a: 1 }),
-      headers: { 'Content-Type': 'application/json' },
-    })
-    assert(response.headers.get('content-type') == 'application/json')
-    assert.deepEqual(await response.json(), { a: 1 })
-  })
-  .case('http://localhost:3000/', async helloHttp => {
-    const response = await helloHttp.options('/')
-    assert(response.headers.get('content-type') == 'application/json')
     assert.deepEqual(await response.json(), { greeting: 'Hello World' })
-  })
-  .case('http://localhost:3000/', async helloHttp => {
-    const response = await helloHttp.trace('/')
-    assert(response.headers.get('content-type') == 'application/json')
-    assert.deepEqual(await response.json(), { greeting: 'Hello World' })
-  })
-  .case('http://localhost:3000/', async helloHttp => {
-    const response = await helloHttp.patch('/', {
+  }
+
+  {
+    const response = await _http.get('/invalid-json')
+    await assert.rejects(
+      response.json(),
+      new SyntaxError('Unterminated string in JSON at position 22 (line 1 column 23)'),
+    )
+    await assert.rejects(
+      response.json(),
+      new SyntaxError('Unterminated string in JSON at position 22 (line 1 column 23)'),
+    )
+  }
+
+  {
+    const response = await _http.post('/echo', {
+      headers: { 'x-test-header': 'testvalue' },
       body: JSON.stringify({ a: 1 }),
-      headers: { 'Content-Type': 'application/json' },
     })
-    assert(response.headers.get('content-type') == 'application/json')
-    assert.deepEqual(await response.json(), { a: 1 })
+    assert.equal(response.headers['x-test-header'], 'testvalue')
+    assert.equal(response.status, 200)
+    const data = await response.json()
+    assert.equal(response.headers['content-type'], 'application/json')
+    assert.equal(Buffer.from(await response.bytes()).toString('utf8'), '{"greeting":"Hello World","a":1}')
+    assert.equal(Buffer.from(await response.bytes()).toString('utf8'), '{"greeting":"Hello World","a":1}')
+    assert.equal(await response.text(), '{"greeting":"Hello World","a":1}')
+    assert.equal(await response.text(), '{"greeting":"Hello World","a":1}')
+    assert.deepEqual(await response.json(), { greeting: 'Hello World', a: 1 })
+    assert.deepEqual(await response.json(), { greeting: 'Hello World', a: 1 })
+  }
+
+  {
+    const response = await _http.put('/echo', {
+      headers: { 'x-test-header': 'testvalue' },
+      body: JSON.stringify({ a: 1 }),
+    })
+    assert.equal(response.headers['x-test-header'], 'testvalue')
+    assert.equal(response.status, 200)
+    const data = await response.json()
+    assert.equal(response.headers['content-type'], 'application/json')
+    assert.equal(Buffer.from(await response.bytes()).toString('utf8'), '{"greeting":"Hello World","a":1}')
+    assert.equal(Buffer.from(await response.bytes()).toString('utf8'), '{"greeting":"Hello World","a":1}')
+    assert.equal(await response.text(), '{"greeting":"Hello World","a":1}')
+    assert.equal(await response.text(), '{"greeting":"Hello World","a":1}')
+    assert.deepEqual(await response.json(), { greeting: 'Hello World', a: 1 })
+    assert.deepEqual(await response.json(), { greeting: 'Hello World', a: 1 })
+  }
+
+  {
+    const response = await _http.patch('/echo', {
+      headers: { 'x-test-header': 'testvalue' },
+      body: JSON.stringify({ a: 1 }),
+    })
+    assert.equal(response.headers['x-test-header'], 'testvalue')
+    assert.equal(response.status, 200)
+    const data = await response.json()
+    assert.equal(response.headers['content-type'], 'application/json')
+    assert.equal(Buffer.from(await response.bytes()).toString('utf8'), '{"greeting":"Hello World","a":1}')
+    assert.equal(Buffer.from(await response.bytes()).toString('utf8'), '{"greeting":"Hello World","a":1}')
+    assert.equal(await response.text(), '{"greeting":"Hello World","a":1}')
+    assert.equal(await response.text(), '{"greeting":"Hello World","a":1}')
+    assert.deepEqual(await response.json(), { greeting: 'Hello World', a: 1 })
+    assert.deepEqual(await response.json(), { greeting: 'Hello World', a: 1 })
+  }
+
+  {
+    const response = await _http.delete('/echo')
+    assert.equal(response.status, 200)
+    const data = await response.json()
+    assert.equal(response.headers['content-type'], 'application/json')
+    assert.equal(Buffer.from(await response.bytes()).toString('utf8'), '{"greeting":"Hello World"}')
+    assert.equal(Buffer.from(await response.bytes()).toString('utf8'), '{"greeting":"Hello World"}')
+    assert.equal(await response.text(), '{"greeting":"Hello World"}')
+    assert.equal(await response.text(), '{"greeting":"Hello World"}')
+    assert.deepEqual(await response.json(), { greeting: 'Hello World' })
+    assert.deepEqual(await response.json(), { greeting: 'Hello World' })
+  }
+
+  {
+    const response = await _http.options('/echo')
+    assert.equal(response.status, 200)
+    const data = await response.json()
+    assert.equal(response.headers['content-type'], 'application/json')
+    assert.equal(Buffer.from(await response.bytes()).toString('utf8'), '{"greeting":"Hello World"}')
+    assert.equal(Buffer.from(await response.bytes()).toString('utf8'), '{"greeting":"Hello World"}')
+    assert.equal(await response.text(), '{"greeting":"Hello World"}')
+    assert.equal(await response.text(), '{"greeting":"Hello World"}')
+    assert.deepEqual(await response.json(), { greeting: 'Hello World' })
+    assert.deepEqual(await response.json(), { greeting: 'Hello World' })
+  }
+
+  {
+    const response = await _http.trace('/echo')
+    assert.equal(response.status, 200)
+    const data = await response.json()
+    assert.equal(response.headers['content-type'], 'application/json')
+    assert.equal(Buffer.from(await response.bytes()).toString('utf8'), '{"greeting":"Hello World"}')
+    assert.equal(Buffer.from(await response.bytes()).toString('utf8'), '{"greeting":"Hello World"}')
+    assert.equal(await response.text(), '{"greeting":"Hello World"}')
+    assert.equal(await response.text(), '{"greeting":"Hello World"}')
+    assert.deepEqual(await response.json(), { greeting: 'Hello World' })
+    assert.deepEqual(await response.json(), { greeting: 'Hello World' })
+  }
+
+  server.close()
+}).case()
+
+const test2 = new Test('HTTP CONNECT', async () => {
+  const proxy = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    res.end('okay')
   })
-  .after(function () {
-    this.httpServer.close()
+  proxy.on('connect', (req, clientSocket, head) => {
+    const { port, hostname } = new URL(`http://${req.url}`)
+    const serverSocket = connect(port || 80, hostname, () => {
+      clientSocket.write('HTTP/1.1 200 Connection Established\r\n' +
+                      'Proxy-agent: Node.js-Proxy\r\n' +
+                      '\r\n')
+      serverSocket.write(head)
+      serverSocket.pipe(clientSocket)
+      clientSocket.pipe(serverSocket)
+    })
   })
+
+  proxy.listen(3001, () => {
+    console.log('proxy listening on port 3001')
+  })
+
+  const p = new Promise((resolve, reject) => {
+    proxy.on('close', resolve)
+    proxy.on('error', reject)
+  })
+
+  const _http = new Http('http://localhost:3001/')
+  const request = _http.connect({ path: 'www.google.com:80' })
+  const chunks = []
+
+  request.on('connect', (res, socket, head) => {
+    console.log('got connected!')
+
+    // Make a request over an HTTP tunnel
+    socket.write('GET / HTTP/1.1\r\n' +
+                 'Host: www.google.com:80\r\n' +
+                 'Connection: close\r\n' +
+                 '\r\n')
+    socket.on('data', (chunk) => {
+      chunks.push(chunk)
+    })
+    socket.on('end', () => {
+      proxy.close()
+    })
+  })
+
+  await p
+
+  assert(chunks.length > 0)
+  assert(chunks[0].toString('utf8').startsWith('HTTP/1.1 200 OK'))
+  proxy.close()
+}).case()
+
+const test = Test.all([
+  test1,
+  test2
+])
+
+if (process.argv[1] == __filename) {
+  test()
+}
+
+module.exports = test
