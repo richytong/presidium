46ef6189331cf7d5d22f466f06836a30c4b091d5
Richard <richytong@gmail.com>
2025-07-02 10:26:45 -0700

    Dynamo -> DynamoDB

--- global.js
+++ global.js
@@ -8,8 +8,17 @@ globalThis.DockerImage = require('./DockerImage')
 globalThis.DockerService = require('./DockerService')
 globalThis.DockerSwarm = require('./DockerSwarm')
 globalThis.DynamoTable = require('./DynamoTable')
+
+globalThis.DynamoDB = {}
+globalThis.DynamoDB.Table = require('./DynamoTable')
+globalThis.DynamoDB.GlobalSecondaryIndex = require('./DynamoIndex')
+globalThis.DynamoDB.GSI = require('./DynamoIndex')
+globalThis.DynamoDB.Stream = require('./DynamoStream')
+
+globalThis.DynamoTable = require('./DynamoTable')
 globalThis.DynamoIndex = require('./DynamoIndex')
 globalThis.DynamoStream = require('./DynamoStream')
+
 globalThis.EC2 = require('./EC2')
 globalThis.ECR = require('./ECR')
 globalThis.Http = require('./Http')
