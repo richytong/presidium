1a7ddbbbf3ac8394d601a1a6a881dc46ca5d503e
Richard <richytong@gmail.com>
2025-07-15 14:56:03 -0700

    parseXML

---
+++ internal/parseXML.js
@@ -0,0 +1,178 @@
+/**
+ * @name _parseTags
+ *
+ * @docs
+ * ```coffeescript [specscript]
+ * attributes Object<string>
+ *
+ * type AST = {
+ *   $name: string,
+ *   $children: Array<string|AST>,
+ *   ...attributes
+ * }
+ *
+ * _parseTags(xml string) -> ast AST
+ * ```
+ */
+function _parseTags(xml) {
+  xml = xml.trim()
+  let i = 0
+
+  const ws   = /\s/
+  const name = /[^\s/>]/
+
+  function skipWS() {
+    while (ws.test(xml[i])) {
+      i++
+    }
+  }
+
+  function node() {
+    if (xml[i] != '<' || xml[i + 1] == '/') {
+      throw new SyntaxError('Malformed tag')
+    }
+
+    i++
+    const tagStart = i
+    while (name.test(xml[i])) {
+      i++
+    }
+
+    // tag name
+    const $name = xml.slice(tagStart, i);
+
+    // attributes
+    const attributes = {}
+
+    skipWS()
+
+    while (xml[i] != '>' && xml[i] != '/') {
+      const keyStart = i
+
+      while (!ws.test(xml[i]) && xml[i] != '=') {
+        i++
+      }
+
+      const key = xml.slice(keyStart, i)
+
+      skipWS()
+
+      if (xml[i] == '=') {
+        i++
+        skipWS()
+
+        const q = xml[i]
+        if (q != '"' && q != "'") {
+          throw SyntaxError('Expected quote for attribute value')
+        }
+
+        i++
+        const valStart = i
+        while (xml[i] != q) {
+          i++
+        }
+
+        attributes[key] = xml.slice(valStart, i)
+        i++
+      }
+
+      skipWS()
+    }
+
+    // self-closing tag
+    if (xml[i] == '/') {
+      i += 2
+      return { $name, ...attributes, $children: [] }
+    }
+
+    i++
+
+    // $children
+    const $children = []
+    let textStart = i
+
+    while (true) {
+      if (xml[i] == '<') {
+
+        if (xml[i + 1] == '/') { // closing tag
+          const text = xml.slice(textStart, i).trim()
+          if (text) {
+            $children.push(text)
+          }
+          i += 2
+
+          const closeStart = i
+          while (xml[i] != '>') {
+            i++
+          }
+          const closeTag = xml.slice(closeStart, i);
+          if (closeTag != $name) {
+            throw SyntaxError(`Mismatched </${closeTag}> – expected </${$name}>`);
+          }
+
+          i++
+          break
+
+        } else { // child element
+          // const text = xml.slice(textStart, i).trim()
+          // console.log('child text', text, { textStart, i })
+          // if (text) {
+          // $children.push(text)
+          // }
+
+          const child = node()
+          $children.push(child)
+          textStart = i
+        }
+
+      } else {
+        i++
+      }
+
+    }
+
+    return { $name, ...attributes, $children }
+  }
+
+  const root = node()
+
+  if (i != xml.length) {
+    throw SyntaxError('Extra content')
+  }
+
+  return root
+}
+
+/**
+ * @name parseXML
+ *
+ * @docs
+ * ```coffeescript [specscript]
+ * ```
+ */
+function parseXML(xml) {
+  xml = xml.replace(/\n/g, '')
+  xml = xml.trim()
+  const ast = {}
+
+  {
+    const mpreamble = /^<\?xml.*\?>/.exec(xml)
+    if (mpreamble) {
+      ast.$preamble = {}
+      const mpairs = mpreamble[0].match(/\w+\=".*?"/g)
+      for (const pair of mpairs) {
+        const [key, quoted] = pair.split('=')
+        ast.$preamble[key] = quoted.replace(/^"/, '').replace(/"$/, '')
+      }
+      xml = xml.replace(/^<\?xml.*\?>/, '')
+    }
+  }
+
+  if (xml) {
+    return { ...ast, ..._parseTags(xml) }
+  }
+
+  return ast
+}
+
+module.exports = parseXML
---
+++ internal/parseXML.test.js
@@ -0,0 +1,149 @@
+const assert = require('assert')
+const parseXML = require('./parseXML')
+
+describe('parseXML', () => {
+  it('Parses the XML optional preamble', async () => {
+    assert.deepEqual(
+      parseXML('<?xml version="1.0" encoding="UTF-8"?>'),
+      { $preamble: { version: '1.0', encoding: 'UTF-8' } }
+    )
+  })
+
+  it('Parses XML tags 1', async () => {
+    assert.deepEqual(
+      parseXML(`
+<Test a="1">
+  <Sub>Content</Sub>
+</Test>
+      `),
+      {
+        $name: 'Test',
+        a: '1',
+        $children: [
+          {
+            $name: 'Sub',
+            $children: ['Content']
+          }
+        ]
+      }
+    )
+
+  })
+
+  it('Parses XML tags 2', async () => {
+    assert.deepEqual(
+      parseXML(
+        '<D><Sub c="3"> <Sub>a</Sub> Content</Sub>  <Sub2 d="/////">    <Sub3 e="nested"> </Sub3>  </Sub2> </D>'
+      ),
+      {
+        $name: 'D',
+        $children: [
+          {
+            $name: 'Sub',
+            c: '3',
+            $children: [
+              {
+                $name: 'Sub',
+                $children: ['a']
+              },
+              'Content'
+            ]
+          },
+          {
+            $name: 'Sub2',
+            d: '/////',
+            $children: [
+              {
+                $name: 'Sub3',
+                e: 'nested',
+                $children: []
+              }
+            ]
+          }
+        ]
+      }
+    )
+  })
+
+  it('Parses XML tags 3', async () => {
+    assert.deepEqual(
+      parseXML(
+        `
+<?xml version="1.0" encoding="UTF-8"?>
+<Root>
+  <LocationConstraint xmlns="http://s3.amazonaws.com/doc/2006-03-01/"/>
+  <Test a="1" b="abc"><Sub c="3">Content</Sub>
+    <Sub2 d="/////">
+      <Sub3 e="nested"></Sub3>
+    </Sub2>
+  </Test>
+</Root>
+        `.trim()
+      ),
+      {
+        $preamble: { version: '1.0', encoding: 'UTF-8' },
+        $name: 'Root',
+        $children: [
+          {
+            $name: 'LocationConstraint',
+            xmlns: 'http://s3.amazonaws.com/doc/2006-03-01/',
+            $children: []
+          },
+          {
+            $name: 'Test',
+            a: '1',
+            b: 'abc',
+            $children: [
+              {
+                $name: 'Sub',
+                c: '3',
+                $children: ['Content']
+              },
+              {
+                $name: 'Sub2',
+                d: '/////',
+                $children: [
+                  {
+                    $name: 'Sub3',
+                    e: 'nested',
+                    $children: []
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    )
+
+  })
+
+  it('Throws SyntaxError for malformed tag', async () => {
+    assert.throws(
+      () => parseXML('asdf'),
+      new SyntaxError('Malformed tag')
+    )
+  })
+
+  it('Throws SyntaxError for unquoted tag', async () => {
+    assert.throws(
+      () => parseXML('<Test a=1 />'),
+      new SyntaxError('Expected quote for attribute value')
+    )
+  })
+
+  it('Throws SyntaxError for mismatched tag', async () => {
+    assert.throws(
+      () => parseXML('<Test></Test2>'),
+      new SyntaxError('Mismatched </Test2> – expected </Test>')
+    )
+  })
+
+  it('Throws SyntaxError for extra content after root tag', async () => {
+    assert.throws(
+      () => parseXML('<Test></Test>extra'),
+      new SyntaxError('Extra content')
+    )
+  })
+
+})
