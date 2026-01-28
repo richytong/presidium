ea5dd7a7cae75e498ff2276009501c37ac58c895
Richard <richytong@gmail.com>
2026-01-28 07:10:15 -0800

    remove base class DynamoDB

--- DynamoDB.js
+++
@@ -1,7 +0,0 @@
-const DynamoDB = {}
-
-DynamoDB.Table = require('./DynamoDBTable')
-DynamoDB.GlobalSecondaryIndex = require('./DynamoDBGlobalSecondaryIndex')
-DynamoDB.Stream = require('./DynamoDBStream')
-
-module.exports = DynamoDB
