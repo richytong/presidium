cb641d31aa6b996dab226a833600568d4ee0aa2d
Richard <richytong@gmail.com>
2026-01-29 17:29:31 -0800

    NpmToken

--- NpmToken.js
+++ NpmToken.js
@@ -1,26 +1,45 @@
-const pipe = require('rubico/pipe')
-const get = require('rubico/get')
+require('rubico/global')
 const fs = require('fs')
+const resolvePath = require('./internal/resolvePath')
 
 /**
  * @name NpmToken
  *
  * @synopsis
  * ```coffeescript [specscript]
- * NpmToken() -> Promise<npmToken string>
+ * NpmToken(options {
+ *   recurse: boolean,
+ * }) -> Promise<npmToken string>
  * ```
  *
  * @description
  * Finds the npm token from `~/.npmrc`
  */
 
-const NpmToken = async function () {
-  return fs.promises.readFile(`${process.env.HOME}/.npmrc`).then(pipe([
-    buffer => buffer.toString().split('\n'),
+const NpmToken = async function (options = {}) {
+  const recurse = options.recurse ?? true
+  const npmrc = '.npmrc'
+  let npmrcDir = resolvePath(process.cwd())
+  let lines = ''
+  while (recurse && npmrcDir != '/') {
+    const npmrcFilePath = resolvePath(npmrcDir, '.npmrc')
+    if (fs.existsSync(npmrcFilePath)) {
+      lines = await fs.promises.readFile(npmrcFilePath)
+        .then(buffer => buffer.toString('utf8').split('\n'))
+      break
+    }
+    npmrcDir = resolvePath(npmrcDir, '..')
+  }
+
+  if (lines == '') {
+    throw new Error('Missing .npmrc file')
+  }
+
+  return pipe(lines, [
     get(0),
     value => (/\/\/registry.npmjs.org\/:_authToken=(.+)/g).exec(value),
     get(1),
-  ]))
+  ])
 }
 
 module.exports = NpmToken
