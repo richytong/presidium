779166bead658b94d737165d038f976e427e1db0
Richard <richytong@gmail.com>
2026-01-29 18:07:19 -0800

    test.js

--- test.js
+++ test.js
@@ -2,39 +2,28 @@ const Test = require('thunk-test')
 
 const test = Test.all([
   require('./Archive.test'),
-  require('./AutoScaling.test'),
-  // require('./AwsCredentials.test'),
-  require('./Dependency.test'),
+  require('./AwsCredentials.test'),
   require('./Docker.test'),
-  require('./DockerContainer.test'),
-  require('./DockerImage.test'),
-  require('./DockerService.test'),
-  require('./DockerSwarm.test'),
-  require('./Dynamo.test'),
-  require('./DynamoIndex.test'),
-  require('./DynamoIndexQueryIterator.test'),
-  require('./DynamoStream.test'),
-  require('./DynamoTable.test'),
-  require('./DynamoTableScanIterator.test'),
-  // require('./EC2.test'),
+  require('./DynamoDBTable.test'),
+  require('./DynamoDBGlobalSecondaryIndex.test'),
+  require('./DynamoDBStream.test'),
   require('./ECR.test'),
-  require('./Elasticsearch.test'),
-  require('./Gzip.test'),
-  require('./Http.test'),
-  require('./HttpServer.test'),
-  require('./Kinesis.test'),
-  require('./KinesisStream.test'),
-  require('./Mongo.test'),
-  require('./MongoCollection.test'),
+  require('./HTTP.test'),
+  require('./NpmToken.test'),
   require('./OptionalValidator.test'),
   require('./Password.test'),
-  require('./S3.test'),
+  require('./Readable.test'),
   require('./S3Bucket.test'),
   require('./SecretsManager.test'),
   require('./StrictValidator.test'),
   require('./TranscribeStream.test'),
-  require('./WebSocketServer.test'),
-  require('./teardown.test'),
+  require('./WebSocket.test'),
+
+  require('./internal/createUpdateServiceSpec.test')
+  require('./internal/StatusCodeMessage.test')
+  require('./internal/handleDynamoDBStreamGetRecordsError.test')
+  require('./internal/StatusCodeMessage.test')
+  require('./internal/pathWalk.test')
 ])
 
 if (process.argv[1] == __filename) {
