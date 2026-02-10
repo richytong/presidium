5db7ef7b78a0f147be8b4854151c7663aed9cc0f
Richard <richytong@gmail.com>
2026-02-09 16:16:26 -0800

    Secrets test env

--- Secrets.test.js
+++ Secrets.test.js
@@ -5,16 +5,16 @@ const Secrets = require('./Secrets')
 describe('Secrets', () => {
   it('Consumes the .secrets file and returns an object of secretName-secretValue pairs', async () => {
     await fs.promises.writeFile('.secrets', `
-MY_VARIABLE_A=abc1
-MY_VARIABLE_B=abc2
+MY_VARIABLE_A=production/abc1
+MY_VARIABLE_B=production/abc2
 MY_VARIABLE_C=abc3
     `)
 
     const secrets = await Secrets()
 
     assert.equal(Object.keys(secrets).length, 3)
-    assert.equal(secrets.MY_VARIABLE_A, 'abc1')
-    assert.equal(secrets.MY_VARIABLE_B, 'abc2')
+    assert.equal(secrets.MY_VARIABLE_A, 'production/abc1')
+    assert.equal(secrets.MY_VARIABLE_B, 'production/abc2')
     assert.equal(secrets.MY_VARIABLE_C, 'abc3')
   })
 })
