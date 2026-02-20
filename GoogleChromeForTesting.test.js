7e0c03798feb5e234db272cc22fbf356eda39ed6
Richard <richytong@gmail.com>
2026-02-20 10:03:34 -0800

    wait for close

--- GoogleChromeForTesting.test.js
+++ GoogleChromeForTesting.test.js
@@ -53,7 +53,17 @@ const test = new Test('GoogleChromeForTesting', async function integration() {
     assert.equal(typeof googleChromeForTesting.devtoolsUrl, 'string')
     assert(googleChromeForTesting.devtoolsUrl.startsWith('ws://'))
 
+    let closeResolve
+    const closePromise = new Promise(_resolve => {
+      closeResolve = _resolve
+    })
+    googleChromeForTesting.on('close', () => {
+      closeResolve()
+    })
+
     googleChromeForTesting.close()
+
+    await closePromise
   }
 
   {
@@ -71,16 +81,16 @@ const test = new Test('GoogleChromeForTesting', async function integration() {
     const closePromise = new Promise(_resolve => {
       closeResolve = _resolve
     })
-    googleChromeForTesting.cmd.on('close', () => {
+    googleChromeForTesting.on('close', () => {
       closeResolve()
     })
 
-    // googleChromeForTesting.close()
+    googleChromeForTesting.close()
 
-    // await closePromise
+    await closePromise
   }
 
-  // await fs.promises.rm('google-chrome-for-testing', { recursive: true, force: true })
+  await fs.promises.rm('google-chrome-for-testing', { recursive: true, force: true })
 
   console.log('Success')
 }).case()
