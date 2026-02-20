1a7b429d5c9e8ebe6f81415f3b3587513b1043dd
Richard <richytong@gmail.com>
2026-02-20 03:50:47 -0800

    test options

--- internal/GoogleChromeForTesting.test.js
+++ internal/GoogleChromeForTesting.test.js
@@ -14,12 +14,12 @@ const test = new Test('GoogleChromeForTesting', async function integration() {
 
   {
     const googleChromeForTesting = new GoogleChromeForTesting()
-    await googleChromeForTesting.init()
-
-    assert.equal(typeof googleChromeForTesting.devtoolsUrl, 'string')
-    assert(googleChromeForTesting.devtoolsUrl.startsWith('ws://'))
-
-    googleChromeForTesting.close()
+    assert.equal(googleChromeForTesting.chromeVersion, 'stable')
+    assert.equal(googleChromeForTesting.chromeDir, `google-chrome-for-testing`)
+    assert.equal(googleChromeForTesting.remoteDebuggingPort, 9222)
+    assert.equal(googleChromeForTesting.headless, false)
+    assert.equal(googleChromeForTesting.userDataDir, 'tmp/chrome')
+    assert.equal(googleChromeForTesting.useMockKeychain, true)
   }
 
   {
@@ -30,6 +30,34 @@ const test = new Test('GoogleChromeForTesting', async function integration() {
       headless: true,
       userDataDir: `${__dirname}/tmp/chrome`,
       useMockKeychain: true,
+    })
+    assert.equal(googleChromeForTesting.chromeVersion, 'stable')
+    assert.equal(googleChromeForTesting.chromeDir, `${__dirname}/google-chrome-for-testing`)
+    assert.equal(googleChromeForTesting.remoteDebuggingPort, 9222)
+    assert.equal(googleChromeForTesting.headless, true)
+    assert.equal(googleChromeForTesting.userDataDir, `${__dirname}/tmp/chrome`)
+    assert.equal(googleChromeForTesting.useMockKeychain, true)
+  }
+
+  {
+    const googleChromeForTesting = new GoogleChromeForTesting({
+      chromeDir: `${__dirname}/google-chrome-for-testing`,
+      userDataDir: `${__dirname}/tmp/chrome`,
+      headless: true,
+    })
+    await googleChromeForTesting.init()
+
+    assert.equal(typeof googleChromeForTesting.devtoolsUrl, 'string')
+    assert(googleChromeForTesting.devtoolsUrl.startsWith('ws://'))
+
+    googleChromeForTesting.close()
+  }
+
+  {
+    const googleChromeForTesting = new GoogleChromeForTesting({
+      chromeDir: `${__dirname}/google-chrome-for-testing`,
+      userDataDir: `${__dirname}/tmp/chrome`,
+      headless: true,
     })
     await googleChromeForTesting.init()
 
