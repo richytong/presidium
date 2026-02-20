ad5e4c6e3cdf2441bfa2549503ae96a2d78ad9f3
Richard <richytong@gmail.com>
2026-02-20 11:23:24 -0800

    tmp

---
+++ tmp.js
@@ -0,0 +1,33 @@
+import UpdateManager from 'stdout-update';
+
+const TICKS = 60;
+const TIMEOUT = 80;
+const manager = UpdateManager.getInstance();
+const frames = ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏'];
+const messages = ['Swapping time and space...', 'Have a good day.', "Don't panic...", 'Updating Updater...', '42'];
+let ticks = TICKS;
+let i = 0;
+
+manager.hook();
+
+// eslint-disable-next-line no-console
+console.log(' - log message');
+// eslint-disable-next-line no-console
+console.error(' - error message');
+// eslint-disable-next-line no-console
+console.warn(' - warn message');
+
+const id = setInterval(() => {
+  if (--ticks < 0) {
+    clearInterval(id);
+    manager.update(['✔ Success', '', 'Messages:', 'this line is be deleted!!!']);
+    manager.erase(1);
+    manager.unhook(false);
+  } else {
+    const frame = frames[(i = ++i % frames.length)];
+    const index = Math.round(ticks / 10) % messages.length;
+    const message = messages[index];
+
+    if (message) manager.update([`${frame} Some process...`, message]);
+  }
+}, TIMEOUT);
