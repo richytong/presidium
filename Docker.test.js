85a1b76f3101697ab9364551d4914eef8853a2e5
Richard <richytong@gmail.com>
2026-02-21 13:23:25 -0800

    delete docker resources and retest gpus

--- Docker.test.js
+++ Docker.test.js
@@ -7,6 +7,10 @@ const sleep = require('./internal/sleep')
 const Readable = require('./Readable')
 
 const test1 = Test('Docker - prune', async function integration() {
+  await docker.deleteNetwork('my-network')
+  await docker.deleteNetwork('my-other-network')
+  await docker.leaveSwarm({ force: true })
+
   const docker = new Docker({ apiVersion: '1.44' })
 
   await docker.pruneContainers()
@@ -454,7 +458,7 @@ const test5 = new Test('Docker - swarm', async function integration() {
       }],
       memory: 256e6, // bytes
       cpus: 0.1,
-      // gpus: 'all',
+      gpus: 'all',
       restart: 'on-failure:5',
 
       healthCmd: ['wget', '--no-verbose', '--tries=1', '--spider', 'localhost:3000'],
@@ -501,7 +505,7 @@ const test5 = new Test('Docker - swarm', async function integration() {
       }],
       memory: 256e6, // bytes
       cpus: 0.1,
-      // gpus: 'all',
+      gpus: 'all',
       restart: 'on-failure:5',
 
       healthCmd: ['wget', '--no-verbose', '--tries=1', '--spider', 'localhost:3000'],
