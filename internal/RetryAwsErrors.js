7db46a058edf85e8488eed5c255218f4ef4cc879
Richard <richytong@gmail.com>
2026-03-27 06:54:45 -0700

    log retryable errors

--- internal/RetryAwsErrors.js
+++ internal/RetryAwsErrors.js
@@ -13,6 +13,7 @@ const RetryAwsErrors = function (func, context, name) {
   return function retriesAwsErrors(...args) {
     return func.apply(context, args).catch(error => {
       if (error.retryable) {
+        console.error(error)
         await sleep(1000)
         return retriesAwsErrors(...args)
       }
