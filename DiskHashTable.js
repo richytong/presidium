1e9eab3f03bef7ebfe3621e10da8a2aabf411eac
Richard <richytong@gmail.com>
2026-04-07 10:54:45 -0700

    DiskHashTable delete + docs

--- DiskHashTable.js
+++ DiskHashTable.js
@@ -5,7 +5,9 @@ const DATA_SLICE_SIZE = 512 * 1024
 const ENCODING = 'utf8'
 
 const EMPTY = 0
+
 const OCCUPIED = 1
+
 const REMOVED = 2
 
 /**
@@ -14,19 +16,55 @@ const REMOVED = 2
  * @docs
  * ```coffeescript [specscript]
  * new DiskHashTable(options {
- *   length: number,
+ *   initialLength: number,
  *   filepath: string,
- * }) -> DiskHashTable
+ * }) -> ht DiskHashTable
+ * ```
+ *
+ * Presidium DiskHashTable class. Creates a hash table that stores all data on disk.
+ *
+ * Arguments:
+ *   * `options`
+ *     * `initialLength` - `number` - the initial length of the disk hash table. Defaults to 1024.
+ *     * `filepath` - `string` - the path to the file used to store the disk hash table data.
+ *
+ * Return:
+ *   * `ht` - [`DiskHashTable`](/docs/DiskHashTable) - a `DiskHashTable` instance.
+ *
+ * ```javascript
+ * const ht = new DiskHashTable({
+ *   initialLength: 1024,
+ *   filepath: '/path/to/data-file',
+ * })
  * ```
  */
 class DiskHashTable {
   constructor(options) {
-    this.length = options.length
+    this.length = options.initialLength ?? 1024
     this.filepath = options.filepath
     this.fd = null
   }
 
-  // init() -> Promise<>
+  /**
+   * @name init
+   *
+   * @docs
+   * ```coffeescript [specscript]
+   * ht.init() -> Promise<>
+   * ```
+   *
+   * Initializes the disk hash table.
+   *
+   * Arguments:
+   *   * (none)
+   *
+   * Return:
+   *   * Empty promise.
+   *
+   * ```javascript
+   * await ht.init()
+   * ```
+   */
   async init() {
     const dir = this.filepath.split('/').slice(0, -1).join('/')
     await fs.promises.mkdir(dir, { recursive: true })
@@ -41,11 +79,50 @@ class DiskHashTable {
     this.fd = await fs.promises.open(this.filepath, 'r+')
   }
 
-  // clear() -> Promise<>
+  /**
+   * @name clear
+   *
+   * @docs
+   * ```coffeescript [specscript]
+   * clear() -> Promise<>
+   * ```
+   *
+   * Clears all data from the disk hash table.
+   *
+   * Arguments:
+   *   * (none)
+   *
+   * Return:
+   *   * Empty promise.
+   *
+   * ```javascript
+   * await ht.clear()
+   * ```
+   */
   async clear() {
     await fs.promises.rm(this.filepath).catch(() => {})
   }
 
+  /**
+   * @name close
+   *
+   * @docs
+   * ```coffeescript [specscript]
+   * close() -> undefined
+   * ```
+   *
+   * Closes the underlying file handle used by the disk hash table.
+   *
+   * Arguments:
+   *   * (none)
+   *
+   * Return:
+   *   * `undefined`
+   *
+   * ```javascript
+   * ht.close()
+   * ```
+   */
   close() {
     this.fd.close()
   }
@@ -70,14 +147,9 @@ class DiskHashTable {
     return prime - (Math.abs(hash) % prime)
   }
 
-  // _getKey(index number) -> key Promise<string>
-  async _getKey(index) {
-    if (index == -1) {
-      throw new Error('Negative index')
-    }
-
+  // _read(index number) -> readBuffer Promise<Buffer>
+  async _read(index) {
     const position = index * DATA_SLICE_SIZE
-
     const readBuffer = Buffer.alloc(DATA_SLICE_SIZE)
 
     await this.fd.read({
@@ -87,6 +159,16 @@ class DiskHashTable {
       length: DATA_SLICE_SIZE,
     })
 
+    return readBuffer
+  }
+
+  // _getKey(index number) -> key Promise<string>
+  async _getKey(index) {
+    if (index == -1) {
+      throw new Error('Negative index')
+    }
+
+    const readBuffer = await this._read(index)
     const statusMarker = readBuffer.readUInt8(0)
     if (statusMarker === EMPTY) {
       return undefined
@@ -97,7 +179,40 @@ class DiskHashTable {
     return keyBuffer.toString(ENCODING)
   }
 
-  // set(key string, value string) -> ()
+  // _setStatusMarker(index number, marker number) -> Promise<>
+  async _setStatusMarker(index, marker) {
+    const position = index * DATA_SLICE_SIZE
+    const buffer = Buffer.alloc(1)
+    buffer.writeUInt8(marker, 0)
+
+    await this.fd.write(buffer, {
+      offset: 0,
+      position,
+      length: buffer.length,
+    })
+  }
+
+  /**
+   * @name set
+   *
+   * @docs
+   * ```coffeescript [specscript]
+   * set(key string, value string) -> Promise<>
+   * ```
+   *
+   * Sets and stores a value by key in the disk hash table.
+   *
+   * Arguments:
+   *   * `key` - `string` - the key to set.
+   *   * `value` - `string` - the value to set corresponding to the key.
+   *
+   * Return:
+   *   * Empty promise.
+   *
+   * ```javascript
+   * await ht.set('my-key', 'my-value')
+   * ```
+   */
   async set(key, value) {
     let index = this._hash1(key)
 
@@ -112,7 +227,7 @@ class DiskHashTable {
 
       index = (index + stepSize) % this.length
       if (index == startIndex) {
-        throw new Error('Hash table is full')
+        throw new Error('Disk hash table is full')
       }
 
       currentKey = await this._getKey(index)
@@ -142,7 +257,26 @@ class DiskHashTable {
     })
   }
 
-  // get(key string) -> value Promise<string>
+  /**
+   * @name get
+   *
+   * @docs
+   * ```coffeescript [specscript]
+   * get(key string) -> value Promise<string>
+   * ```
+   *
+   * Gets a value by key from the disk hash table.
+   *
+   * Arguments:
+   *   * `key` - `string` - the key corresponding to the value.
+   *
+   * Return:
+   *   * `value` - `string` - the value corresponding to the key.
+   *
+   * ```javascript
+   * const value = await ht.get('my-key')
+   * ```
+   */
   async get(key) {
     let index = this._hash1(key)
     const startIndex = index
@@ -166,38 +300,76 @@ class DiskHashTable {
       return undefined
     }
 
-    const position = index * DATA_SLICE_SIZE
-
-    const readBuffer = Buffer.alloc(DATA_SLICE_SIZE)
-
-    await this.fd.read({
-      buffer: readBuffer,
-      offset: 0,
-      position,
-      length: DATA_SLICE_SIZE,
-    })
-
+    const readBuffer = await this._read(index)
     const statusMarker = readBuffer.readUInt8(0)
-    // TODO handle status marker
-
-    const keyByteLength = readBuffer.readUInt32BE(1)
-    const valueByteLength = readBuffer.readUInt32BE(5)
-    const keyBuffer = readBuffer.subarray(9, keyByteLength + 9)
-    const valueBuffer = readBuffer.subarray(
-      9 + keyByteLength,
-      9 + keyByteLength + valueByteLength
-    )
-    return valueBuffer.toString(ENCODING)
-  }
-
-  /*
-  async remove(key) {
-    let index = this._hash1(key)
-    if (this.keyMap[index]) {
-      this.keyMap[index] = this.keyMap[index].filter(pair => pair[0] !== key)
+    if (statusMarker === OCCUPIED) {
+      const keyByteLength = readBuffer.readUInt32BE(1)
+      const valueByteLength = readBuffer.readUInt32BE(5)
+      const keyBuffer = readBuffer.subarray(9, keyByteLength + 9)
+      const valueBuffer = readBuffer.subarray(
+        9 + keyByteLength,
+        9 + keyByteLength + valueByteLength
+      )
+      return valueBuffer.toString(ENCODING)
     }
+
+    return undefined
   }
-  */
+
+  /**
+   * @name delete
+   *
+   * @docs
+   * ```coffeescript [specscript]
+   * delete(key string) -> didDelete Promise<boolean>
+   * ```
+   *
+   * Deletes a key and corresponding value from the disk hash table.
+   *
+   * Arguments:
+   *   * `key` - `string` - the key to delete.
+   *
+   * Return:
+   *   * `didDelete` - `boolean` - a promise of whether the key and corresponding value was deleted.
+   *
+   * ```javascript
+   * const didDelete = await ht.delete('my-key')
+   * ```
+   */
+  async delete(key) {
+    let index = this._hash1(key)
+    const startIndex = index
+    const stepSize = this._hash2(key)
+
+    let currentKey = await this._getKey(index)
+    console.log('delete', { key, currentKey, index })
+    while (currentKey) {
+      if (key == currentKey) {
+        break
+      }
+
+      index = (index + stepSize) % this.length
+      if (index == startIndex) {
+        return false // entire table searched
+      }
+
+      currentKey = await this._getKey(index)
+    }
+
+    if (currentKey == null) {
+      return false
+    }
+
+    const readBuffer = await this._read(index)
+    const statusMarker = readBuffer.readUInt8(0)
+
+    if (statusMarker === OCCUPIED) {
+      await this._setStatusMarker(index, REMOVED)
+      return true
+    }
+
+    return false
+  }
 
 }
 
--- DiskHashTable.test.js
+++ DiskHashTable.test.js
@@ -5,7 +5,7 @@ const DiskHashTable = require('./DiskHashTable')
 const test1 = new Test('DiskHashTable', async function integration1() {
   const ht1024 = new DiskHashTable({
     filepath: `${__dirname}/DiskHashTable_test_data/1024`,
-    length: 1024,
+    initialLength: 1024,
   })
   await ht1024.clear()
   await ht1024.init()
@@ -20,12 +20,17 @@ const test1 = new Test('DiskHashTable', async function integration1() {
 
   await ht1024.set('maroon', '#800___')
   assert.equal(await ht1024.get('maroon'), '#800___')
+  await ht1024.delete('maroon').then(didDelete => assert(didDelete))
+  assert.strictEqual(await ht1024.get('maroon'), undefined)
+  await ht1024.set('maroon', '#800000')
+  assert.equal(await ht1024.get('maroon'), '#800000')
 
   assert.strictEqual(await ht1024.get('notfound'), undefined)
+  await ht1024.delete('notfound').then(didDelete => assert(!didDelete))
 
   const ht1 = new DiskHashTable({
     filepath: `${__dirname}/DiskHashTable_test_data/1`,
-    length: 1,
+    initialLength: 1,
   })
   await ht1.clear()
   await ht1.init()
@@ -35,7 +40,7 @@ const test1 = new Test('DiskHashTable', async function integration1() {
 
   await assert.rejects(
     ht1.set('yellow', '#FFFF00'),
-    new Error('Hash table is full')
+    new Error('Disk hash table is full')
   )
 
   assert.strictEqual(await ht1024.get('notfound'), undefined)
@@ -52,7 +57,7 @@ const test1 = new Test('DiskHashTable', async function integration1() {
 const test1_1 = new Test('DiskHashTable', async function integration1_1() {
   const ht2 = new DiskHashTable({
     filepath: `${__dirname}/DiskHashTable_test_data/2`,
-    length: 2,
+    initialLength: 2,
   })
   await ht2.clear()
   await ht2.init()
@@ -63,6 +68,12 @@ const test1_1 = new Test('DiskHashTable', async function integration1_1() {
   await ht2.set(collisionKey, '#800000(1)')
   assert.equal(await ht2.get('maroon'), '#800000')
   assert.equal(await ht2.get(collisionKey), '#800000(1)')
+  await ht2.delete('maroon').then(didDelete => assert(didDelete))
+  await ht2.delete('maroon').then(didDelete => assert(!didDelete))
+  assert.equal(await ht2.get('maroon'), undefined)
+  await ht2.delete(collisionKey).then(didDelete => assert(didDelete))
+  assert.equal(await ht2.get(collisionKey), undefined)
+  await ht2.delete('maroon3').then(didDelete => assert(!didDelete))
 
   ht2.close()
 }).case()
@@ -70,7 +81,7 @@ const test1_1 = new Test('DiskHashTable', async function integration1_1() {
 const test1_2 = new Test('DiskHashTable', async function integration1_2() {
   const ht3 = new DiskHashTable({
     filepath: `${__dirname}/DiskHashTable_test_data/3`,
-    length: 3,
+    initialLength: 3,
   })
   await ht3.clear()
   await ht3.init()
