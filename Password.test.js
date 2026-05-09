30282d8b0be7047e4e6f920e966f7b1f01e4209f
Richard <richytong@gmail.com>
2026-05-09 10:08:13 -0700

    Password coverage

--- Password.test.js
+++ Password.test.js
@@ -12,6 +12,36 @@ const test = new Test('Password', async function integration() {
   )
 
   await Password.verify(plaintext, hashed) // should not throw
+
+  const crypto = require('crypto')
+
+  cryptoScrypt = crypto.scrypt
+  crypto.scrypt = (text, salt, n, cb) => {
+    cb(new Error('test'))
+  }
+
+  await assert.rejects(
+    Password.hash(plaintext),
+    new Error('test')
+  )
+
+  let calls = 0
+  crypto.scrypt = (text, salt, n, cb) => {
+    if (calls === 0) {
+      calls += 1
+      return cryptoScrypt(text, salt, n, cb)
+    } else {
+      cb(new Error('test'))
+    }
+  }
+
+  await assert.rejects(
+    Password.verify(plaintext, hashed),
+    new Error('test')
+  )
+
+  crypto.scrypt = cryptoScrypt
+
 }).case()
 
 if (process.argv[1] == __filename) {
